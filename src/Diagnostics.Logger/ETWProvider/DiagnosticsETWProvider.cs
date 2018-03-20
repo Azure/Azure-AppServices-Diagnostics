9546ab1912ea12ed7d614741b56512dc02e64a09
ShekharGupta1988 <shgup@microsoft.com>
2018-03-20 15:47:29 -0700

    Fix Admin ETW Events by adding mandatory Message Parameter

--- src/Diagnostics.Logger/ETWProvider/DiagnosticsETWProvider.cs
+++ src/Diagnostics.Logger/ETWProvider/DiagnosticsETWProvider.cs
@@ -12,13 +12,13 @@ namespace Diagnostics.Logger
 
         #region Compile Host Events (ID Range : 1000 - 1999)
 
-        [Event(1000, Level = EventLevel.Informational, Channel = EventChannel.Admin)]
+        [Event(1000, Level = EventLevel.Informational, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogCompilerHostMessage)]
         public void LogCompilerHostMessage(string Message)
         {
             WriteDiagnosticsEvent(1000, Message);
         }
 
-        [Event(1001, Level = EventLevel.Error, Channel = EventChannel.Admin)]
+        [Event(1001, Level = EventLevel.Error, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogCompilerHostUnhandledException)]
         public void LogCompilerHostUnhandledException(string RequestId, string Source, string ExceptionType, string ExceptionDetails)
         {
             WriteDiagnosticsEvent(1001,
@@ -28,7 +28,7 @@ namespace Diagnostics.Logger
                 ExceptionDetails);
         }
 
-        [Event(1002, Level = EventLevel.Informational, Channel = EventChannel.Admin)]
+        [Event(1002, Level = EventLevel.Informational, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogCompilerHostAPISummary)]
         public void LogCompilerHostAPISummary(string RequestId, string Address, string Verb, int StatusCode, long LatencyInMilliseconds, string StartTime, string EndTime)
         {
             WriteDiagnosticsEvent(1002,
@@ -45,13 +45,13 @@ namespace Diagnostics.Logger
 
         #region Runtime Host Events (ID Range : 2000 - 2499)
 
-        [Event(2000, Level = EventLevel.Informational, Channel = EventChannel.Admin)]
+        [Event(2000, Level = EventLevel.Informational, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogRuntimeHostMessage)]
         public void LogRuntimeHostMessage(string Message)
         {
             WriteDiagnosticsEvent(2000, Message);
         }
 
-        [Event(2001, Level = EventLevel.Error, Channel = EventChannel.Admin)]
+        [Event(2001, Level = EventLevel.Error, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogRuntimeHostUnhandledException)]
         public void LogRuntimeHostUnhandledException(string RequestId, string Source, string SubscriptionId, string ResourceGroup, string Resource, string ExceptionType, string ExceptionDetails)
         {
             WriteDiagnosticsEvent(2001,
@@ -64,7 +64,7 @@ namespace Diagnostics.Logger
                 ExceptionDetails);
         }
 
-        [Event(2002, Level = EventLevel.Informational, Channel = EventChannel.Admin)]
+        [Event(2002, Level = EventLevel.Informational, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogRuntimeHostAPISummary)]
         public void LogRuntimeHostAPISummary(string RequestId, string SubscriptionId, string ResourceGroup, string Resource, string Address, string Verb, string OperationName, int StatusCode, long LatencyInMilliseconds, string StartTime, string EndTime)
         {
             WriteDiagnosticsEvent(2002,
@@ -80,24 +80,24 @@ namespace Diagnostics.Logger
                 StartTime,
                 EndTime);
         }
-        
+
         #endregion
 
         #region SourceWatcher Events (ID Range : 2500 - 2599)
 
-        [Event(2500, Level = EventLevel.Informational, Channel = EventChannel.Admin)]
+        [Event(2500, Level = EventLevel.Informational, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogSourceWatcherMessage)]
         public void LogSourceWatcherMessage(string Source, string Message)
         {
             WriteDiagnosticsEvent(2500, Source, Message);
         }
 
-        [Event(2501, Level = EventLevel.Warning, Channel = EventChannel.Admin)]
+        [Event(2501, Level = EventLevel.Warning, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogSourceWatcherWarning)]
         public void LogSourceWatcherWarning(string Source, string Message)
         {
             WriteDiagnosticsEvent(2501, Source, Message);
         }
 
-        [Event(2502, Level = EventLevel.Error, Channel = EventChannel.Admin)]
+        [Event(2502, Level = EventLevel.Error, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogSourceWatcherException)]
         public void LogSourceWatcherException(string Source, string Message, string ExceptionType, string ExceptionDetails)
         {
             WriteDiagnosticsEvent(2502, Source, Message, ExceptionType, ExceptionDetails);
@@ -107,13 +107,13 @@ namespace Diagnostics.Logger
 
         #region Compiler Host Client Events (ID Range: 2600 - 2699)
 
-        [Event(2600, Level = EventLevel.Informational, Channel = EventChannel.Admin)]
+        [Event(2600, Level = EventLevel.Informational, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogCompilerHostClientMessage)]
         public void LogCompilerHostClientMessage(string RequestId, string Source, string Message)
         {
             WriteDiagnosticsEvent(2600, RequestId, Source, Message);
         }
 
-        [Event(2601, Level = EventLevel.Error, Channel = EventChannel.Admin)]
+        [Event(2601, Level = EventLevel.Error, Channel = EventChannel.Admin, Message = ETWMessageTemplates.LogCompilerHostClientException)]
         public void LogCompilerHostClientException(string RequestId, string Source, string Message, string ExceptionType, string ExceptionDetails)
         {
             WriteDiagnosticsEvent(2601, Source, Message, ExceptionType, ExceptionDetails);
