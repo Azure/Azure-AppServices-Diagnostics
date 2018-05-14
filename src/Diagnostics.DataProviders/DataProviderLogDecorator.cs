6adf48a34773b477a2fe32ba012928ef567b3059
Hawk Foreste <hawfor@microsoft.com>
2018-05-14 15:02:52 -0700

    Log for kusto and geomaster data providers

--- src/Diagnostics.DataProviders/DataProviderLogDecorator.cs
+++ src/Diagnostics.DataProviders/DataProviderLogDecorator.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Text;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Diagnostics.Logger;
 using Diagnostics.ModelsAndUtils.Models;
 
 namespace Diagnostics.DataProviders
@@ -31,58 +32,58 @@ namespace Diagnostics.DataProviders
 
         public Task<DataTable> ExecuteQuery(string query, string stampName, string requestId = null, string operationName = null)
         {
-            return _kustoDataProvider.ExecuteQuery(query, stampName, requestId, operationName);
+            return MakeDependencyCall(requestId, _kustoDataProvider.ExecuteQuery(query, stampName, requestId, operationName));
         }
 
         public Task<List<IDictionary<string, dynamic>>> GetAppDeployments(string subscriptionId, string resourceGroupName, string name)
         {
-            return _geomasterDataProvider.GetAppDeployments(subscriptionId, resourceGroupName, name);
+            return MakeDependencyCall(null, _geomasterDataProvider.GetAppDeployments(subscriptionId, resourceGroupName, name));
         }
 
         public Task<IDictionary<string, string>> GetAppSettings(string subscriptionId, string resourceGroupName, string name)
         {
-            return _geomasterDataProvider.GetAppSettings(subscriptionId, resourceGroupName, name);
+            return MakeDependencyCall(null, _geomasterDataProvider.GetAppSettings(subscriptionId, resourceGroupName, name));
         }
 
         public Task<IDictionary<string, string[]>> GetStickySlotSettingNames(string subscriptionId, string resourceGroupName, string name)
         {
-            return _geomasterDataProvider.GetStickySlotSettingNames(subscriptionId, resourceGroupName, name);
+            return MakeDependencyCall(null, _geomasterDataProvider.GetStickySlotSettingNames(subscriptionId, resourceGroupName, name));
         }
 
         public Task<VnetValidationRespone> VerifyHostingEnvironmentVnet(string subscriptionId, string vnetResourceGroup, string vnetName, string vnetSubnetName, CancellationToken cancellationToken = default(CancellationToken))
         {
-            return _geomasterDataProvider.VerifyHostingEnvironmentVnet(subscriptionId, vnetResourceGroup, vnetName, vnetSubnetName, cancellationToken);
+            return MakeDependencyCall(null, _geomasterDataProvider.VerifyHostingEnvironmentVnet(subscriptionId, vnetResourceGroup, vnetName, vnetSubnetName, cancellationToken));
         }
 
-        private Task<T> MakeDependencyCall<T>(string dataProviderOperation, Task<T> dataProviderTask)
+        private async Task<T> MakeDependencyCall<T>(string requestId, Task<T> dataProviderTask, [CallerMemberName]string dataProviderOperation = "")
         {
             Exception dataProviderException = null;
+            DateTime startTime = DateTime.UtcNow, endTime;
             try
             {
-
+                return await dataProviderTask;
             }catch(Exception ex)
             {
                 dataProviderException = ex;
+                throw;
             }
             finally
             {
-                
+                endTime = DateTime.UtcNow;
+                var latencyMilliseconds = Convert.ToInt64((endTime - startTime).TotalMilliseconds);
+
                 if (dataProviderException == null)
                 {
-
+                    DiagnosticsETWProvider.Instance.LogDataProviderException(requestId ?? "", dataProviderOperation, 
+                        startTime.ToString("HH:mm:ss.fff"), endTime.ToString("HH:mm:ss.fff"), 
+                        latencyMilliseconds, dataProviderException.GetType().ToString(), dataProviderException.ToString());
                 }
                 else
                 {
-
+                    DiagnosticsETWProvider.Instance.LogDataProviderOperationSummary(requestId ?? "", dataProviderOperation, startTime.ToString("HH:mm:ss.fff"), 
+                        endTime.ToString("HH:mm:ss.fff"), latencyMilliseconds);
                 }
             }
-
-            return null;
-        }
-
-        private Task<T> Run<T>(Func<object, T> dataProvider)
-        {
-            return null;
         }
     }
 }
