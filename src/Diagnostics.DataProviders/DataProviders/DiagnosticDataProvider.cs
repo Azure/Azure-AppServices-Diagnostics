1759de73f9af30282d7b4a14c871f86b8ff353be
Puneet Gupta <puneetg@microsoft.com>
2018-05-24 22:56:53 +0530

    Adding Kusto Queries to Detector Response

--- src/Diagnostics.DataProviders/DataProviders/DiagnosticDataProvider.cs
+++ src/Diagnostics.DataProviders/DataProviders/DiagnosticDataProvider.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Diagnostics.ModelsAndUtils.Models;
 
 namespace Diagnostics.DataProviders
-{
+{    
     public class DiagnosticDataProvider
     {
         private OperationDataCache _cache;
 
         public DiagnosticDataProvider(OperationDataCache cache)
         {
-            _cache = cache;
+            _cache = cache;            
         }
 
         protected Task<T> GetOrAddFromCache<T>(string key, Func<string, CacheMember> addFunction)
         {
             return Convert.ChangeType(_cache.GetOrAdd(key, addFunction), typeof(Task<T>)) as Task<T>;
         }
+
+        public DataProviderMetadata Metadata { get; set; }
     }
 }
--- src/Diagnostics.DataProviders/DataProviders/KustoDataProvider.cs
+++ src/Diagnostics.DataProviders/DataProviders/KustoDataProvider.cs
@@ -5,25 +5,49 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Diagnostics.DataProviders
 {
+    public class KustoQuery
+    {
+        public string QueryText;
+        public string KustoEndpoint;
+        public string QueryUrl;
+    }
     public class KustoDataProvider: DiagnosticDataProvider, IDiagnosticDataProvider
     {
         private KustoDataProviderConfiguration _configuration;
         private IKustoClient _kustoClient;
 
-        public KustoDataProvider(OperationDataCache cache, KustoDataProviderConfiguration configuration): base(cache)
+        public KustoDataProvider(OperationDataCache cache, KustoDataProviderConfiguration configuration) : base(cache)
         {
             _configuration = configuration;
             _kustoClient = KustoClientFactory.GetKustoClient(configuration);
+            Metadata = new DataProviderMetadata
+            {
+                ProviderName = "Kusto"
+            };
         }
 
-        public async Task<DataTable> ExecuteQuery(string query, string stampName, string requestId = null, string operationName = null)
+        public async Task<DataTable> ExecuteQuery(string query, string stampName, string requestId = null, [CallerMemberName] string operationName = null)
         {
+            AddQueryInformationToMetadata(query);
             return await _kustoClient.ExecuteQueryAsync(query, stampName, requestId, operationName);
+        }
+
+        private void AddQueryInformationToMetadata(string query)
+        {
+            KustoQuery q = new KustoQuery
+            {
+                QueryText = query,
+                KustoEndpoint = "",
+                QueryUrl = ""
+            };
+
+            Metadata.PropertyBag.Add(new KeyValuePair<string, object>("Query", q));
         }
     }
 }
