82757892b0b9ba008997f1054e6e960b4f265839
Puneet Gupta <puneetg@microsoft.com>
2018-05-25 18:55:53 +0530

    Adding logic to get the KUSTO query URL

--- src/Diagnostics.DataProviders/DataProviders/KustoDataProvider.cs
+++ src/Diagnostics.DataProviders/DataProviders/KustoDataProvider.cs
@@ -4,18 +4,20 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace Diagnostics.DataProviders
 {
     public class KustoQuery
     {
-        public string QueryText;
-        public string KustoEndpoint;
-        public string QueryUrl;
+        public string Text;        
+        public string Url;
     }
     public class KustoDataProvider: DiagnosticDataProvider, IDiagnosticDataProvider
     {
@@ -34,20 +36,88 @@ namespace Diagnostics.DataProviders
 
         public async Task<DataTable> ExecuteQuery(string query, string stampName, string requestId = null, [CallerMemberName] string operationName = null)
         {
-            AddQueryInformationToMetadata(query);
+            AddQueryInformationToMetadata(query, stampName);
             return await _kustoClient.ExecuteQueryAsync(query, stampName, requestId, operationName);
         }
 
-        private void AddQueryInformationToMetadata(string query)
+        private void AddQueryInformationToMetadata(string query, string stampName)
         {
             KustoQuery q = new KustoQuery
             {
-                QueryText = query,
-                KustoEndpoint = "",
-                QueryUrl = ""
+                Text = query,               
+                Url = CreateKustoQueryUri(stampName, query)
             };
 
             Metadata.PropertyBag.Add(new KeyValuePair<string, object>("Query", q));
+        }
+
+        private string ParseRegionFromStamp(string stampName)
+        {
+            if (string.IsNullOrWhiteSpace(stampName))
+            {
+                throw new ArgumentNullException("stampName");
+            }
+
+            var stampParts = stampName.Split(new char[] { '-' });
+            if (stampParts.Any() && stampParts.Length >= 3)
+            {
+                return stampParts[2];
+            }
+
+            //return * for private stamps if no prod stamps are found
+            return "*";
+        }
+
+        private string CreateKustoQueryUri(string stampName, string query)
+        {
+            string kustoClusterName = null;
+            try
+            {
+                string appserviceRegion = ParseRegionFromStamp(stampName);
+
+                if (!_configuration.RegionSpecificClusterNameCollection.TryGetValue(appserviceRegion.ToLower(), out kustoClusterName))
+                {
+                    if (!_configuration.RegionSpecificClusterNameCollection.TryGetValue("*", out kustoClusterName))
+                    {
+                        throw new KeyNotFoundException(String.Format("Kusto Cluster Name not found for Region : {0}", appserviceRegion.ToLower()));
+                    }
+                }
+
+                string encodedQuery = EncodeQueryAsBase64Url(query);
+
+                var url = string.Format("https://web.kusto.windows.net/clusters/{0}.kusto.windows.net/databases/{1}?q={2}", kustoClusterName, _configuration.DBName, encodedQuery);
+
+                return url;
+            }
+            catch (Exception ex)
+            {
+                string message = string.Format("stamp : {0}, kustoClusterName : {1}, Exception : {2}",
+                    stampName ?? "null",
+                    kustoClusterName ?? "null",
+                    ex.ToString());
+                throw;
+            }
+        }
+
+        // From Kusto.Data.Common.CslCommandGenerator.EncodeQueryAsBase64Url
+        private string EncodeQueryAsBase64Url(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+            {
+                return string.Empty;
+            }
+            byte[] bytes = Encoding.UTF8.GetBytes(query);
+            string result;
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (GZipStream gZipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+                {
+                    gZipStream.Write(bytes, 0, bytes.Length);
+                }
+                memoryStream.Seek(0L, SeekOrigin.Begin);
+                result = HttpUtility.UrlEncode(Convert.ToBase64String(memoryStream.ToArray()));
+            }
+            return result;
         }
     }
 }
