aaf2ae289db5bdc97814bd78b341ebafebd3f2ee
stevenernst131 <sterns@microsoft.com>
2018-06-13 16:13:34 -0700

    Add Category to definition

---
+++ src/Diagnostics.ModelsAndUtils/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diagnostics.ModelsAndUtils.Models
+{
+    public class Category 
+    {
+        private Category(string categoryName)
+        {
+            Value = categoryName;
+        }
+
+        public string Value { get; private set; }
+
+        public static Category Custom(string name)
+        {
+            return new Category(name);
+        }
+
+        public static readonly Category AvailabilityAndPerformance = new Category("Availability and Performance");
+        public static readonly Category ConfigurationAndManagement = new Category("Configuration and Management");
+    }
+}
