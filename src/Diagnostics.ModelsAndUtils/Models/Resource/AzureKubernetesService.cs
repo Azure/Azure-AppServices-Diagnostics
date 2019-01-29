fce374c6c7637100a3fb88c6fca52961ae970fd1
nmallick1 <nmallick@microsoft.com>
2019-01-29 13:07:03 -0800

    First Commit

---
+++ src/Diagnostics.ModelsAndUtils/Models/Resource/AzureKubernetesService.cs
@@ -0,0 +1,61 @@
+﻿using Diagnostics.ModelsAndUtils.Attributes;
+
+namespace Diagnostics.ModelsAndUtils.Models
+{
+    /// <summary>
+    /// Resource representing Azure Kubernetes Service
+    /// </summary>
+    public class AzureKubernetesService : AzureKubernetesServiceFilter, IResource
+    {
+        /// <summary>
+        /// Name of the Azure Kubernetes Service
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Subscription Id(Guid)
+        /// </summary>
+        public string SubscriptionId { get; set; }
+
+        /// <summary>
+        /// Resource Group Name
+        /// </summary>
+        public string ResourceGroup { get; set; }
+
+        public string Provider
+        {
+            get
+            {
+                return "Microsoft.ContainerService";
+            }
+        }
+
+        /// <summary>
+        /// Name of Resource Type as defined by ARM resource id. Examples: 'sites', 'hostingEnvironments'
+        /// </summary>
+        public string ResourceTypeName
+        {
+            get
+            {
+                return "managedClusters";
+            }
+        }
+
+        /// <summary>
+        /// Determines whether the Azure Kubernetes Service resource is applicable after filtering.
+        /// </summary>
+        /// <param name="filter">Resource Filter</param>
+        /// <returns>True, if resource passes the filter. False otherwise</returns>
+        public bool IsApplicable(IResourceFilter filter)
+        {
+            return filter is AzureKubernetesServiceFilter;
+        }
+
+        public AzureKubernetesService(string subscriptionId, string resourceGroup, string appName) : base()
+        {
+            this.SubscriptionId = subscriptionId;
+            this.ResourceGroup = resourceGroup;
+            this.Name = appName;
+        }
+    }
+}
