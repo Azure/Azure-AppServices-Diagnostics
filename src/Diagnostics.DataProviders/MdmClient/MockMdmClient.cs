ce1734fe4e3a61d58c5e38205afb96c7ef1a3cad
Yan Xue <3491507+crazyxy@users.noreply.github.com>
2019-01-28 16:07:03 -0800

    MDM data provider (#101)
    * Data provider for MDM
    
    * Add MDM client
    
    * Code review feedback
    
    * code review feedback
    
    * code review feedback

---
+++ src/Diagnostics.DataProviders/MdmClient/MockMdmClient.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Diagnostics.DataProviders.Interfaces;
+
+namespace Diagnostics.DataProviders
+{
+    /// <summary>
+    /// Mock MDM client.
+    /// </summary>
+    internal class MockMdmClient : IMdmClient
+    {
+        /// <summary>
+        /// Gets the list of namespaces for the monitoringAccount.
+        /// </summary>
+        /// <param name="monitoringAccount">The monitoring account.</param>
+        /// <returns>The list of namespaces for the monitoringAccount.</returns>
+        public Task<IEnumerable<string>> GetNamespacesAsync(string monitoringAccount)
+        {
+            IEnumerable<string> namespaces = new string[]
+            {
+                "namespace1",
+                "namespace2",
+                "namespace3"
+            };
+
+            return Task.FromResult(namespaces);
+        }
+
+        /// <summary>
+        /// Gets the list of metric names for the monitoringAccount and metricNamespace.
+        /// </summary>
+        /// <param name="monitoringAccount">The monitoring account.</param>
+        /// <param name="metricNamespace">The metric namespace.</param>
+        /// <returns>The list of metric names for the monitoringAccount and metricNamespace.</returns>
+        public Task<IEnumerable<string>> GetMetricNamesAsync(string monitoringAccount, string metricNamespace)
+        {
+            IEnumerable<string> metric = new string[]
+            {
+                "metric1",
+                "metric2",
+                "metric3"
+            };
+
+            return Task.FromResult(metric);
+        }
+
+        /// <summary>
+        /// Gets the list of dimension names for the metricId.
+        /// </summary>
+        /// <param name="monitoringAccount">Monitoring account.</param>
+        /// <param name="metricNamespace">Metric namespace</param>
+        /// <param name="metricName">Metric name</param>
+        /// <returns>The list of dimension names for the metricId.</returns>
+        public Task<IEnumerable<string>> GetDimensionNamesAsync(string monitoringAccount, string metricNamespace, string metricName)
+        {
+            IEnumerable<string> dimensions = new string[]
+            {
+                "dimension1",
+                "dimension2",
+                "dimension3"
+            };
+
+            return Task.FromResult(dimensions);
+        }
+
+        /// <summary>
+        /// Gets the dimension values for dimensionName satifying the dimensionFilters and
+        /// </summary>
+        /// <param name="metricId">Metric id.</param>
+        /// <param name="filter">The dimension filters representing the pre-aggregate dimensions. Create an emtpy include filter for dimension with no filter values. Requested dimension should also be part of this and should be empty.</param>
+        /// <param name="dimensionName">Name of the dimension for which values are requested.</param>
+        /// <param name="startTimeUtc">Start time for evaluating dimension values.</param>
+        /// <param name="endTimeUtc">End time for evaluating dimension values.</param>
+        /// <returns>Dimension values for dimensionName.</returns>
+        public Task<IEnumerable<string>> GetDimensionValuesAsync(MetricIdentifier metricId, List<Tuple<string, IEnumerable<string>>> filter, string dimensionName, DateTime startTimeUtc, DateTime endTimeUtc)
+        {
+            IEnumerable<string> value = new string[]
+            {
+                "value1",
+                "value2",
+                "value3"
+            };
+
+            return Task.FromResult(value);
+        }
+
+        /// <summary>
+        /// Gets a list of the time series, each with multiple sampling types.
+        /// </summary>
+        /// <param name="startTimeUtc">The start time UTC.</param>
+        /// <param name="endTimeUtc">The end time UTC.</param>
+        /// <param name="samplingTypes">The sampling types.</param>
+        /// <param name="definitions">The time series definitions.</param>
+        /// <param name="seriesResolutionInMinutes">The resolution window used to reduce the resolution of the returned series.</param>
+        /// <param name="aggregationType">The aggregation function used to reduce the resolution of the returned series.</param>
+        /// <returns>The time series of for the given definitions.</returns>
+        public Task<IEnumerable<TimeSeries<MetricIdentifier, double?>>> GetMultipleTimeSeriesAsync(DateTime startTimeUtc, DateTime endTimeUtc, SamplingType[] samplingTypes, IEnumerable<TimeSeriesDefinition<MetricIdentifier>> definitions, int seriesResolutionInMinutes = 1, AggregationType aggregationType = AggregationType.Automatic)
+        {
+            IEnumerable<TimeSeries<MetricIdentifier, double?>> series = new TimeSeries<MetricIdentifier, double?>[] { };
+            return Task.FromResult(series);
+        }
+
+    }
+}
---
+++ src/Diagnostics.DataProviders/Utility/SerializationUtils.cs
@@ -0,0 +1,121 @@
+﻿using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Diagnostics.DataProviders.Utility
+{
+    /// <summary>
+    /// Set of functions used for metrics serialization and deserialization.
+    /// </summary>
+    internal static class SerializationUtils
+    {
+        /// <summary>
+        /// Reads int value stored in Base-128 encoding.
+        /// </summary>
+        /// <param name="reader">Binary reader to be used for reading.</param>
+        /// <returns>Read value.</returns>
+        public static int ReadInt32FromBase128(BinaryReader reader)
+        {
+            return (int)ReadInt64FromBase128(reader);
+        }
+
+        /// <summary>
+        /// Reads long value stored in Base-128 encoding.
+        /// </summary>
+        /// <param name="reader">Binary reader to be used for reading.</param>
+        /// <returns>Read value.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long ReadInt64FromBase128(BinaryReader reader)
+        {
+            var dummy = 0;
+            return ReadInt64FromBase128(reader, ref dummy);
+        }
+
+        /// <summary>
+        /// Reads long value stored in Base-128 encoding.
+        /// </summary>
+        /// <param name="reader">Binary reader to be used for reading.</param>
+        /// <param name="bytesRead">The number that is incremented each time a single byte is read.</param>
+        /// <returns>Read value.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static long ReadInt64FromBase128(BinaryReader reader, ref int bytesRead)
+        {
+            long val = 0;
+            var shift = 0;
+            byte b;
+            var first = true;
+            var negative = false;
+            do
+            {
+                if (first)
+                {
+                    first = false;
+                    b = reader.ReadByte();
+                    bytesRead++;
+                    val += (b & 0x3f) << shift;
+                    negative = (b & 0x40) != 0;
+                    shift += 6;
+                }
+                else
+                {
+                    b = reader.ReadByte();
+                    bytesRead++;
+                    val += (long)(b & 0x7f) << shift;
+                    shift += 7;
+                }
+            }
+            while ((b & 0x80) != 0);
+            return negative ? -val : val;
+        }
+
+        /// <summary>
+        /// Reads uint value stored in Base-128 encoding.
+        /// </summary>
+        /// <param name="reader">Binary reader to be used for reading.</param>
+        /// <returns>Read value.</returns>
+        public static uint ReadUInt32FromBase128(BinaryReader reader)
+        {
+            return (uint)ReadUInt64FromBase128(reader);
+        }
+
+        /// <summary>
+        /// Reads ulong value stored in Base-128 encoding.
+        /// </summary>
+        /// <param name="reader">Binary reader to be used for reading.</param>
+        /// <returns>Read value.</returns>
+        public static ulong ReadUInt64FromBase128(BinaryReader reader)
+        {
+            ulong val = 0;
+            var shift = 0;
+            byte b;
+            do
+            {
+                b = reader.ReadByte();
+                val = val + ((ulong)(b & 0x7f) << shift);
+                shift += 7;
+            }
+            while ((b & 0x80) != 0);
+            return val;
+        }
+
+        /// <summary>
+        /// Reads ulong value stored in Base-128 encoding.
+        /// </summary>
+        /// <param name="buffer">Buffer from which value to be read.</param>
+        /// <param name="offset">Offset in buffer to start reading from.</param>
+        /// <returns>Read value.</returns>
+        public static ulong ReadUInt64FromBase128(byte[] buffer, ref int offset)
+        {
+            ulong val = 0;
+            var shift = 0;
+            byte b;
+            do
+            {
+                b = buffer[offset++];
+                val = val + ((ulong)(b & 0x7f) << shift);
+                shift += 7;
+            }
+            while ((b & 0x80) != 0);
+            return val;
+        }
+    }
+}
