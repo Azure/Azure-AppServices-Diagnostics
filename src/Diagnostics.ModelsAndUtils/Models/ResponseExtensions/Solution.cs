e13ca754bdf24d4d0bbbc85d6b4a697858159df0
Daniel Grooms <daniel.grooms@microsoft.com>
2019-02-13 18:02:46 -0800

    Simplified backend Solution

--- src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/Solution.cs
+++ src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/Solution.cs
@@ -8,50 +8,25 @@ using System.Web;
 
 namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
 {
-    public enum SolutionActionType
-    {
-        Internal,
-        External
-    }
-
-    public interface IHttpResponse
-    {
-        string Body { get; set; }
-        string ResponseCode { get; set; }
-        int StatusCode { get; set; }
-    }
-
     public class Solution
     {
-        public string Title;
-        public IEnumerable<string> Descriptions;
-        // In case confirmation is needed for "instant" actions like restart, which could down the service
-        public bool RequiresConfirmation;
-        public SolutionActionType ActionType;
+        public string Title { get; set; }
+        public IEnumerable<string> Descriptions { get; set; }
+        public Uri ResourceUri { get; set; }
+        public bool RequiresConfirmation { get; set; }
 
-        // Option 1: Call an azure operation (Ex. Restart Service)
-        // Not sure what this would require, maybe it uses http call? 
-        // TODO: Research ARM interface
-
-        // Option 2: Navigate to an Azure blade
-        // Need resource group, resource name, blade name
-        // Internally wire these to make a url the user can click
-        public string ResourceGroup;
-        public string ResourceName;
-        public string BladeName;
-
-        // Complete action via API - uri, body, verb, etc.
-        public Uri ActionUri;
-        public string ContentType;
-        public string Content;
-        public IHttpResponse Response;
-
-        public Solution(string title, IEnumerable<string> descriptions, bool confirm = false)
+        public Solution(string title, IEnumerable<string> descriptions, Uri resourceUri, bool confirm = false)
         {
             Title = title;
             Descriptions = descriptions;
+            ResourceUri = resourceUri;
             RequiresConfirmation = confirm;
         }
+
+        public Solution(string title, IEnumerable<string> descriptions, string resourceUri, bool confirm = false) : 
+            this(title, descriptions, new Uri(resourceUri), confirm) { }
+
+        //TODO: Another overload taking ResourceGroup and SiteName to build resourceUri would also be useful
     }
 
     public static class ResponseSolutionExtension
@@ -63,45 +38,8 @@ namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
             foreach(var label in new List<string>() {
                 nameof(Solution.Title),
                 nameof(Solution.Descriptions),
-                nameof(Solution.RequiresConfirmation),
-                nameof(Solution.ResourceGroup),
-                nameof(Solution.ResourceName),
-                nameof(Solution.BladeName) })
-            {
-                table.Columns.Add(new DataColumn(label, typeof(string)));
-            }
-
-            table.Rows.Add(new object[]
-            {
-                solution.Title,
-                // Strange antipattern
-                JsonConvert.SerializeObject(solution.Descriptions),
-                solution.RequiresConfirmation,
-                solution.ResourceGroup,
-                solution.ResourceName,
-                solution.BladeName
-            });
-
-            var diagData = new DiagnosticData()
-            {
-                Table = table,
-                RenderingProperties = new Rendering(RenderingType.Solution)
-            };
-
-            response.Dataset.Add(diagData);
-            return diagData;
-        }
-
-        public static DiagnosticData AddSolution(this Response response, Insight forInsight, Solution solution)
-        {
-            var table = new DataTable();
-            foreach (var label in new List<string>() {
-                nameof(Solution.Title),
-                nameof(Solution.Descriptions),
-                nameof(Solution.RequiresConfirmation),
-                nameof(Solution.ResourceGroup),
-                nameof(Solution.ResourceName),
-                nameof(Solution.BladeName)
+                nameof(Solution.ResourceUri),
+                nameof(Solution.RequiresConfirmation)
             })
             {
                 table.Columns.Add(new DataColumn(label, typeof(string)));
@@ -112,10 +50,8 @@ namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
                 solution.Title,
                 // Strange antipattern
                 JsonConvert.SerializeObject(solution.Descriptions),
-                solution.RequiresConfirmation,
-                solution.ResourceGroup,
-                solution.ResourceName,
-                solution.BladeName
+                solution.ResourceUri.ToString(),
+                solution.RequiresConfirmation
             });
 
             var diagData = new DiagnosticData()
