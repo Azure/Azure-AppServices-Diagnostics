96d364a3f372c2a804c5772a2cebd47cbde8ed68
Hawk Foreste <hawfor@microsoft.com>
2019-02-15 16:02:40 -0800

    Resolve wawsobsever json response property names to post body model properties

--- src/Diagnostics.RuntimeHost/Models/PostBodyModel.cs
+++ src/Diagnostics.RuntimeHost/Models/PostBodyModel.cs
@@ -2,24 +2,48 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Diagnostics.RuntimeHost.Models
 {
     public class DiagnosticSiteData
     {
         public string Name { get; set; }
+
         public string Tags { get; set; }
+
         public string Kind { get; set; }
+
         public string Stack { get; set; }
+
+        [JsonProperty("namespace_descriptor")]
         public string NamespaceDescriptor { get; set; }
+
+        [JsonProperty("is_default_container")]
         public bool IsDefaultContainer { get; set; }
+
+        [JsonProperty("is_linux")]
         public bool? IsLinux { get; set; }
+
+        [JsonProperty("default_host_name")]
         public string DefaultHostName { get; set; }
+
+        [JsonProperty("scm_site_hostname")]
         public string ScmSiteHostname { get; set; }
+
+        [JsonProperty("modified_time_utc")]
         public DateTime? ModifiedTimeUtc { get; set; }
+
+        [JsonProperty("web_space")]
         public string WebSpace { get; set; }
+
+        [JsonProperty("host_names")]
         public IEnumerable<DiagnosticHostnameData> HostNames { get; set; }
+
+        [JsonProperty("resource_group")]
         public string ResourceGroup { get; set; }
+
+
         public DiagnosticStampData Stamp { get; set; }
     }
 
@@ -33,14 +57,28 @@ namespace Diagnostics.RuntimeHost.Models
     public class DiagnosticStampData
     {
         public string Name { get; set; }
+
+        [JsonProperty("internal_name")]
         public string InternalName { get; set; }
+
+        [JsonProperty("service_address")]
         public string ServiceAddress { get; set; }
+
         public int State { get; set; }
+
+        [JsonProperty("dns_suffix")]
         public string DnsSuffix { get; set; }
+
         public DiagnosticStampType Kind { get; set; }
+
         public string Tags { get; set; }
+
+        [JsonProperty("unhealthy_since")]
         public DateTime? UnhealthySince { get; set; }
+
+        [JsonProperty("suspended_on")]
         public DateTime? SuspendedOn { get; set; }
+
         public bool IsUnhealthy
         {
             get
