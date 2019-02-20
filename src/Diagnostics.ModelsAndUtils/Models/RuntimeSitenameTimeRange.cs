48b19dbd153196d2b8cfeb4e0d6e88e9916f2bec
Hawk Foreste <hawfor@microsoft.com>
2019-02-20 09:22:06 -0800

    Revert these files. Instead made change in observer API to serialize the json properties without underscores.

--- src/Diagnostics.ModelsAndUtils/Models/RuntimeSitenameTimeRange.cs
+++ src/Diagnostics.ModelsAndUtils/Models/RuntimeSitenameTimeRange.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using Newtonsoft.Json;
 
 namespace Diagnostics.ModelsAndUtils.Models
 {
     public class RuntimeSitenameTimeRange
     {
-        [JsonProperty("runtime_sitename")]
         public string RuntimeSitename { get; set; }
-
-        [JsonProperty("start_time")]
         public DateTime StartTime { get; set; }
-
-        [JsonProperty("end_time")]
         public DateTime EndTime { get; set; }
     }
 }
