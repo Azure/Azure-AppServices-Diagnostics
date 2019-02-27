e901490a90171358abd83174e6d157bcb2a1e2dd
Yan Xue <xuey@microsoft.com>
2019-02-27 08:20:06 -0800

    Update copyright

--- src/Diagnostics.Logger/ETWProvider/DiagnosticsETWProvider.cs
+++ src/Diagnostics.Logger/ETWProvider/DiagnosticsETWProvider.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="DiagnosticsETWProvider.cs" company="Microsoft">
-// Copyright (c) Microsoft. All rights reserved.
+﻿// <copyright file="DiagnosticsETWProvider.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 // </copyright>
 
--- src/Diagnostics.Logger/ETWProvider/DiagnosticsEventSourceBase.cs
+++ src/Diagnostics.Logger/ETWProvider/DiagnosticsEventSourceBase.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="DiagnosticsEventSourceBase.cs" company="Microsoft">
-// Copyright (c) Microsoft. All rights reserved.
+﻿// <copyright file="DiagnosticsEventSourceBase.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 // </copyright>
 
