4ac9e060eda08f13a2a5b873cd454f3219a6b9d6
nmallick1 <nmallick@microsoft.com>
2019-04-03 14:30:17 -0700

    Adding support for a generic ARM resource at backend

---
+++ src/Diagnostics.ModelsAndUtils/Attributes/ResourceFilter/ArmResourceFilter.cs
@@ -0,0 +1,14 @@
+﻿namespace Diagnostics.ModelsAndUtils.Attributes
+{
+    public class ArmResourceFilter : ResourceFilterBase
+    {
+        public string Provider { get; set; }
+        public string ResourceTypeName { get; set; }
+
+        public ArmResourceFilter(string provider, string resourceTypeName, bool internalOnly = true) : base(ResourceType.ArmResource, internalOnly)
+        {
+            this.Provider = provider;
+            this.ResourceTypeName = resourceTypeName;
+        }
+    }
+}
---
+++ src/Diagnostics.ModelsAndUtils/Models/Resource/ArmResource.cs
@@ -0,0 +1,35 @@
+﻿using Diagnostics.ModelsAndUtils.Attributes;
+using Diagnostics.ModelsAndUtils.ScriptUtilities;
+
+namespace Diagnostics.ModelsAndUtils.Models
+{
+    /// <summary>
+    /// Resource representing a Generic Arm Resource
+    /// </summary>
+    public class ArmResource : ArmResourceFilter, IResource
+    {
+        public string SubscriptionId { get; set; }
+        public string ResourceGroup { get; set; }
+        public string Name { get; set; }
+
+        public string ResourceUri
+        {
+            get
+            {
+                return UriUtilities.BuildAzureResourceUri(SubscriptionId, ResourceGroup, Name, Provider, ResourceTypeName);
+            }
+        }
+
+        public bool IsApplicable(IResourceFilter filter)
+        {
+            return filter is ArmResource;
+        }
+
+        public ArmResource(string subscriptionId, string resourceGroup, string provider, string resourceTypeName, string resourceName) : base(provider, resourceTypeName)
+        {
+            this.SubscriptionId = subscriptionId;
+            this.ResourceGroup = resourceGroup;
+            this.Name = resourceName;
+        }
+    }
+}
