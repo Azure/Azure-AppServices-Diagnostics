d85e04f18c097ac3de2dc3e57d3cbc48c4f9164e
Ajay Sharma <ajsharm@microsoft.com>
2019-05-16 13:28:53 -0700

    Handle local sourcewatcher case

--- src/Diagnostics.AIProjects/SearchAPI/RegistryReader.py
+++ src/Diagnostics.AIProjects/SearchAPI/RegistryReader.py
@@ -7,8 +7,12 @@ try:
 	aKey = OpenKey(aReg, r"SOFTWARE\Microsoft\IIS Extensions\Web Hosting Framework")
 	sourceWatcherKey = OpenKey(aKey, "SourceWatcher")
 	dataProvidersKey = OpenKey(aKey, "DiagnosticDataProviders")
-	githubKey = OpenKey(sourceWatcherKey,"Github")
-	githubFolderPath = QueryValueEx(githubKey, "DestinationScriptsPath")[0]
+	try:
+		githubKey = OpenKey(sourceWatcherKey,"Github")
+		githubFolderPath = QueryValueEx(githubKey, "DestinationScriptsPath")[0]
+	except:
+		githubKey = OpenKey(sourceWatcherKey,"Local")
+		githubFolderPath = QueryValueEx(githubKey, "DestinationScriptsPath")[0]
 	kustoKey = OpenKey(dataProvidersKey, "Kusto")
 	kustoClientId = QueryValueEx(kustoKey, "ClientId")[0]
 	kustoClientSecret = QueryValueEx(kustoKey, "AppKey")[0]
