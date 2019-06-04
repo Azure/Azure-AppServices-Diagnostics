f659c9eb9f9b1210fe49b9a94aea82b2615840fc
Craig Maher <cmaher@microsoft.com>
2019-06-04 16:56:23 -0700

    making travic CI work (hopefully)

--- tests/DataProviderTests/DataProviderTests.cs
+++ tests/DataProviderTests/DataProviderTests.cs
@@ -208,11 +208,11 @@ namespace Diagnostics.Tests.DataProviderTests
             var kustoHeartBeatService = new KustoHeartBeatService(config.KustoConfiguration);
             
             MockKustoClient.ShouldHeartbeatSucceed = false;
-            await Task.Delay(500);
+            await Task.Delay(1000);
             Assert.Equal(config.KustoConfiguration.KustoClusterFailoverGroupings, kustoHeartBeatService.GetClusterNameFromStamp("waws-prod-mockstamp"));
 
             MockKustoClient.ShouldHeartbeatSucceed = true;
-            await Task.Delay(500);
+            await Task.Delay(1000);
             Assert.Equal(config.KustoConfiguration.KustoClusterNameGroupings, kustoHeartBeatService.GetClusterNameFromStamp("waws-prod-mockstamp"));
 
         }
