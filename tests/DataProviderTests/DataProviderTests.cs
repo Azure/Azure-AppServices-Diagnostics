d2d4ee1064e5555a099d0b0ae981984890174581
Craig Maher <cmaher@microsoft.com>
2019-06-04 17:01:11 -0700

    more guesses

--- tests/DataProviderTests/DataProviderTests.cs
+++ tests/DataProviderTests/DataProviderTests.cs
@@ -208,11 +208,11 @@ namespace Diagnostics.Tests.DataProviderTests
             var kustoHeartBeatService = new KustoHeartBeatService(config.KustoConfiguration);
             
             MockKustoClient.ShouldHeartbeatSucceed = false;
-            await Task.Delay(1000);
+            await Task.Delay(2000);
             Assert.Equal(config.KustoConfiguration.KustoClusterFailoverGroupings, kustoHeartBeatService.GetClusterNameFromStamp("waws-prod-mockstamp"));
 
             MockKustoClient.ShouldHeartbeatSucceed = true;
-            await Task.Delay(1000);
+            await Task.Delay(2000);
             Assert.Equal(config.KustoConfiguration.KustoClusterNameGroupings, kustoHeartBeatService.GetClusterNameFromStamp("waws-prod-mockstamp"));
 
         }
