8d0d9d8e33c8442c25ce92c0d5addb2d123597e1
Cindy Peng <38216903+cindylovescoding@users.noreply.github.com>
2019-06-12 01:58:53 -0700

    Add example

--- src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/ApplicationInsight.cs
+++ src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/ApplicationInsight.cs
@@ -27,6 +27,16 @@ namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
 
     public class AppInsightsOperationContext
     {
+        /// <summary>
+        /// This defines the application insights analysis metadata with query that will be executed and rendered in external portal.
+        /// </summary>
+        /// <param name="title">Application insights analysis title</param>
+        /// <param name="description">Application insights analysis description</param>
+        /// <param name="query">The query string to run against application insight instance</param>
+        /// <param name="portalBladeInfo">The blade to suggest to customer for this analysis</param>
+        /// <param name="renderingProperties">The rendering properties to render the application insight query result</param>
+        /// <returns>AppInsightsOperationContext Object</returns>
+        /// <example>
         public AppInsightsOperationContext(string title, string description, string query, BladeInfo portalBladeInfo, Rendering renderingProperties)
         {
             this.Title = title;
@@ -63,51 +73,34 @@ namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
     public static class ApplicationInsight
     {
         /// <summary>
-        /// Adds an ASC Insight that will be rendered as a regular insight.
-        /// It will flow automatically to Azure Support Center if your detector is enabled for a support topic.
+        /// Adds application Insight analysis metadata list.
         /// </summary>
         /// <param name="res">Response object for extension method</param>
-        /// <param name="title">Title of insight. This should be a constant string, without any part of it changing depending on the resource.</param>
-        /// <param name="status">Status of the insight. All insight status types are available, but None and Success with be changes to Info in Azure Support Center.</param>
-        /// <param name="description">The description of the insight. This should not contain anything that is instructing the reader to do anything.
-        /// It should only expand on any relevant information about the insight.</param>
-        /// <param name="recommendedAction">Recommended action that is specifically addressing what steps thesupport engineer should take.
-        /// If you only want to have the support engineer to send the customer ready content, then that is all you would have to include here.</param>
-        /// <param name="customerReadyContent">This is a response that is meant for the support engineer to paste directly into an email to the customer.
-        /// It is meant to completely solve the problem, and if that is not the case you may not want to use this field, by passing a null value.
-        /// Other good information to have is additional resources for the customer so that they can solve the problem in the future.</param>
-        /// <param name="context">Operation context which is passed into detector run method.</param>
-        /// <param name="ascOnly">Only show the insight in Azure Support Center.</param>
-        /// <param name="extraNameValuePairs">Additional name value pairs that you want to display in Applens/App Service Diagnostics. These will not be added in Azure Support Center.
-        /// For markdown, wrap your text in markdown tags. HTML or plain text also allowed</param>
-        /// <param name="isExpanded">Whether you want to Applens/App Service Diagnostics to expand the insight initially.</param>
-        /// <returns>Azure Support Center Insight Object</returns>
+        /// <param name="applicationInsightsList">A list of AppInsightsOperationContext object, each object defines the application insight analysis metadata.</param>
+        /// <returns>DiagnosticData Object</returns>
         /// <example>
-        /// This sample shows how to use the <see cref="AddAscInsight"/> method.
+        /// This sample shows how to add application insight analysis <see cref="AddApplicationInsightsViewList"/> method.
         /// <code>
         /// public async static Task<![CDATA[<Response>]]> Run(DataProviders dp, OperationContext cxt, Response res)
         /// {
-        ///     var descriptionMarkdown =
-        ///         @"###A scale operation failed because there was already a scale operation underway. Here is the error:
-        ///
-        ///           ```
-        ///           Operation failed because a current scale operation is ongoing.
-        ///           ```";
-        ///
-        ///     var recommendedActionPlainText = "Copy and paste the Customer Ready Content and send to the customer";
-        ///
-        ///     var customerReadyActionMarkdown =
-        ///         @$"Your scale operation for site ***{cxt.Resource.Name}*** failed because there was a current scale operation underway.
-        ///
-        ///            Please wait for the current operation to finish and then try again to scale your app service plan. ";
-        ///
-        ///     res.AddAscInsight(
-        ///         "Failed Scale Operation Detected",
-        ///         InsightStatus.Critical,
-        ///         new Text(descriptionMarkdown, true),
-        ///         new Text(recommendedActionPlainText),
-        ///         new Text(customerReadyActionMarkdown, true),
-        ///         cxt);
+        ///      BladeInfo appInsightsFailuresBlade = new BladeInfo("FailuresCuratedFrameBlade", "Open Application Insight Failures Blade");
+        ///     
+        ///     var appInsightsOperationContextException = new AppInsightsOperationContext(
+        ///         "Application Exceptions that occured during this time period",
+        ///         "",
+        ///         @"exceptions 
+        ///         | where timestamp > ago(24h) 
+        ///         | where client_Type == 'PC'
+        ///         | summarize Count = count() by outerMessage, problemId, type
+        ///         | top 5 by Count desc
+        ///         | project Message = outerMessage, Exception = problemId, Count",
+        ///        appInsightsFailuresBlade,
+        ///        new Rendering(RenderingType.Table));
+        ///        
+        ///     var appInsightslist = new List<AppInsightsOperationContext> { appInsightsOperationContextException};
+        ///     res.AddApplicationInsightsViewList(appInsightslist);
+        ///     
+        ///     return res;
         /// }
         /// </code>
         /// </example>
