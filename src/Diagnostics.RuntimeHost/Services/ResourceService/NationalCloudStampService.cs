6305397c5a66d802ceb1b5a06984cc43160faa5f
Hawk Foreste <hawfor@microsoft.com>
2019-07-02 02:02:18 -0700

    Create new stamp service for national cloud

---
+++ src/Diagnostics.RuntimeHost/Services/ResourceService/NationalCloudStampService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Diagnostics.DataProviders;
+using Diagnostics.ModelsAndUtils.Attributes;
+
+namespace Diagnostics.RuntimeHost.Services
+{
+    public sealed class NationalCloudStampService : StampService
+    {
+        protected override async Task<List<string>> GetTenantIdsAsync(string stamp, DateTime startTime, DateTime endTime, DataProviderContext dataProviderContext, PlatformType platformType)
+        {
+            if (platformType == PlatformType.Windows)
+            {
+                return await base.GetTenantIdsAsync(stamp, startTime, endTime, dataProviderContext, platformType);
+            }
+            else
+            {
+                return await Task.FromResult(new List<string>());
+            }
+        }
+
+        protected override string GetTenantIdQuery(string stamp, string startTimeStr, string endTimeStr, string tableName)
+        {
+            return
+                $@"{RoleInstanceHeartBeatTableName}
+                | where TIMESTAMP >= datetime({startTimeStr}) and TIMESTAMP <= datetime({endTimeStr}) and where EventStampName =~ ""{stamp}""
+                | summarize by Tenant, PublicHost";
+        }
+    }
+}
