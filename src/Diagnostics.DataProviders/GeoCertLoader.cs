70c9273f1c9be3f411ba26b4f6e8fa55754e75f9
Rekha Swaminathan <reswamin@microsoft.com>
2019-08-09 14:15:09 -0700

    Use cert to talk to Geo

---
+++ src/Diagnostics.DataProviders/GeoCertLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Configuration;
+
+namespace Diagnostics.DataProviders
+{
+    public class GeoCertLoader
+    {
+        private static readonly Lazy<GeoCertLoader> _instance = new Lazy<GeoCertLoader>(() => new GeoCertLoader());
+
+        public static GeoCertLoader Instance => _instance.Value;
+
+        public X509Certificate2 GeoCert { get; private set; }
+        
+        public void LoadCertFromKeyVault(IConfiguration configuration)
+        {
+            try
+            {
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                var prodKeyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                var certificateSecret = prodKeyVaultClient.GetSecretAsync($"https://{configuration["Secrets:ProdKeyVaultName"]}.vault.azure.net", configuration["GeoMaster:CertificateName"]).Result;
+                var privateKeyBytes = Convert.FromBase64String(certificateSecret.Value);
+                GeoCert = new X509Certificate2(privateKeyBytes);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
