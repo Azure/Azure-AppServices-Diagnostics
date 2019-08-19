746b829a5c67276a1486ea71d77aa96759d81c17
Daniel Grooms <dagrooms52@users.noreply.github.com>
2019-08-19 11:57:58 -0700

    Remove global suppression, too specific

--- src/Diagnostics.DataProviders/GlobalSuppressions.cs
+++ src/Diagnostics.DataProviders/GlobalSuppressions.cs
@@ -2,5 +2,3 @@
 // attributes that are applied to this project.
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "<Pending>", Scope = "member", Target = "~M:Diagnostics.DataProviders.SupportObserverDataProviderBase.GetResource(System.String)~System.Threading.Tasks.Task{}")]
