18936bc1dc75a1d50dab310e9fa0309058c12b94
Craig Maher <cmaher@microsoft.com>
2019-10-29 13:42:18 -0700

    revert some refactoring that is not needed

--- src/Diagnostics.ModelsAndUtils/Attributes/Definition.cs
+++ src/Diagnostics.ModelsAndUtils/Attributes/Definition.cs
@@ -55,11 +55,39 @@ namespace Diagnostics.ModelsAndUtils.Attributes
         [DataMember]
         public IEnumerable<SupportTopic> SupportTopicList { get; set; }
 
+        [JsonIgnore]
+        public string AnalysisType { get; set; } = string.Empty;
+
         /// <summary>
         /// Gets Analysis Types for which this detector should apply to.
         /// </summary>
         [DataMember]
-        public List<string> AnalysisTypes { get; set; }
+        public List<string> AnalysisTypes
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(AnalysisType))
+                {
+                    return null;
+                }
+                else
+                {
+                    return AnalysisType.Split(',').ToList();
+                }
+            }
+
+            set
+            {
+                if (value == null)
+                {
+                    AnalysisType = string.Empty;
+                }
+                else
+                {
+                    AnalysisType = string.Join(",", value);
+                }
+            }
+        }
 
         /// <summary>
         /// Whether this detector is an Analysis Detector or not.
