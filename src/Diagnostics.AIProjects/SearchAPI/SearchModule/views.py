b15e0c6322285986867a1ff606a2732372be05d7
Ajay Sharma <ajsharm@microsoft.com>
2019-11-07 15:45:23 -0800

    Multiple query for testing and filter detectors in training

--- src/Diagnostics.AIProjects/SearchAPI/SearchModule/views.py
+++ src/Diagnostics.AIProjects/SearchAPI/SearchModule/views.py
@@ -108,6 +108,28 @@ def queryDetectorsMethod():
     res = json.dumps(results)
     return (res, 200)
 
+@app.route('/queryMultiple', methods=["POST"])
+def queryMultipleMethod():
+    data = json.loads(request.data.decode('utf-8'))
+    requestId = data['requestId']
+
+    txts = data['texts']
+    if not txts:
+        return ("No texts provided for search", 400)
+    productid = getProductId(data)
+    if not productid:
+        return ('Resource data not available', 404)
+    productid = productid[0]
+    try:
+        loadModel(productid)
+    except Exception as e:
+        loggerInstance.logHandledException(requestId, e)
+        loggerInstance.logToFile(requestId, e)
+        return (json.dumps({"query": txts, "results": []}), 200)
+    
+    res = json.dumps([loaded_models[productid].queryDetectors(txt_data) for txt_data in txts])
+    return (res, 200)
+
 @app.route('/queryUtterances', methods=["POST"])
 @cross_origin()
 @authProvider()
