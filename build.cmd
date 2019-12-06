4c31900bfadf367881e18d6dffbd7d6f4b1ebba3
yuegu <yuegu@microsoft.com>
2019-12-06 11:50:07 -0800

    address comments

--- build.cmd
+++ build.cmd
@@ -6,7 +6,7 @@ rem Build and publish the solution
 dotnet build --no-incremental --no-restore "%slnFile%" /p:DeployOnBuild=true /p:WebPublishMethod=Package /p:PublishProfile=Release /p:PackageAsSingleFile=true --output "%output%"
 if %errorlevel% neq 0 (
     popd
-    echo "Failed to build solution correctly. Error level is %ERRORLEVEL%"
+    echo "Failed to build and publish the solution correctly. Error level is %ERRORLEVEL%"
     exit /B %errorlevel%
 )
 
