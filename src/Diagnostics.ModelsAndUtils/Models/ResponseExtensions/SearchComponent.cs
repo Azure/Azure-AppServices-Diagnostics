5dcf85ae766ab4bfd242d509b8da7386adc431db
Ajay Sharma <ajsharm@microsoft.com>
2019-12-10 11:19:04 -0800

    Adding a new rendering type for search component

---
+++ src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/SearchComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
+{
+    public static class ResponseSearchComponentExtension
+    {
+        public static DiagnosticData AddSearch(this Response response)
+        {
+            var table = new DataTable();
+            var diagData = new DiagnosticData()
+            {
+                Table = table,
+                RenderingProperties = new Rendering(RenderingType.SearchComponent)
+            };
+            response.Dataset.Add(diagData);
+            return diagData;
+        }
+    }
+}
