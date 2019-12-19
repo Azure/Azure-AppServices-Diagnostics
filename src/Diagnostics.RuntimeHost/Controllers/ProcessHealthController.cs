e7ca848494670a61cc80049b256baca1e9100869
Ajay Sharma <ajsharm@microsoft.com>
2019-12-19 15:13:09 -0800

    Adding an outbound network connectivity check in runtime host

--- src/Diagnostics.RuntimeHost/Controllers/ProcessHealthController.cs
+++ src/Diagnostics.RuntimeHost/Controllers/ProcessHealthController.cs
@@ -5,6 +5,8 @@ using Diagnostics.RuntimeHost.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Diagnostics.RuntimeHost.Services.SourceWatcher;
 using System.Threading.Tasks;
+using System.Net;
+using Microsoft.Extensions.Configuration;
 
 namespace Diagnostics.RuntimeHost.Controllers
 {
@@ -13,18 +15,44 @@ namespace Diagnostics.RuntimeHost.Controllers
     {
         private ISourceWatcherService _sourceWatcherService;
         private ICompilerHostClient _compilerHostClient;
+        private IConfiguration _configuration;
 
-        public ProcessHealthController(ISourceWatcherService sourceWatcherService, ICompilerHostClient compilerHostClient)
+        public ProcessHealthController(ISourceWatcherService sourceWatcherService, ICompilerHostClient compilerHostClient, IConfiguration Configuration)
         {
             // These dependencies are injected for the services to start.
             _sourceWatcherService = sourceWatcherService;
             _compilerHostClient = compilerHostClient;
+            _configuration = Configuration;
+        }
+
+        public static bool PingExternal(string pingUrl)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (client.OpenRead(pingUrl))
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         [HttpGet(UriElements.HealthPing)]
         public async Task<IActionResult> HealthPing()
         {
             await _sourceWatcherService.Watcher.WaitForFirstCompletion();
+
+            bool isCheckEnabled = Convert.ToBoolean(_configuration["OutboundConnectivity:IsCheckEnabled"]);
+            if (isCheckEnabled)
+            {
+                bool externalPing = PingExternal(_configuration["OutboundConnectivity:PingUrl"]);
+                if (!externalPing)
+                {
+                    return StatusCode(500);
+                }
+            }
             return Ok("Server is up and running.");
         }
     }
