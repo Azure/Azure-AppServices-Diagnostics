e0e2c41d169308c63efafd2c4ad494e5722b92e7
Rekha Swaminathan <reswamin@microsoft.com>
2020-01-13 11:39:49 -0800

    Creating overloaded method that accepts additionalParams

--- src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/DetectorCollection.cs
+++ src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/DetectorCollection.cs
@@ -5,6 +5,22 @@ namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
 {
     public static class ResponseDetectorViewExtensions
     {
+        public static DiagnosticData AddDetectorCollection(this Response response, List<string> detectorIds)
+        {
+            var diagnosticData = new DiagnosticData()
+            {
+                RenderingProperties = new DetectorCollectionRendering()
+                {
+                    DetectorIds = detectorIds,
+                    AdditionalParams = string.Empty,
+                }
+            };
+
+            response.Dataset.Add(diagnosticData);
+
+            return diagnosticData;
+        }
+
         public static DiagnosticData AddDetectorCollection(this Response response, List<string> detectorIds, IDictionary<string, string> additionalParams = null)
         {
             var diagnosticData = new DiagnosticData()
@@ -21,7 +37,24 @@ namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
             return diagnosticData;
         }
 
-        public static DiagnosticData AddDetector(this Response response, string detectorId, IDictionary<string, string> additionalParams = null )
+
+        public static DiagnosticData AddDetector(this Response response, string detectorId)
+        {
+            var diagnosticData = new DiagnosticData()
+            {
+                RenderingProperties = new DetectorCollectionRendering()
+                {
+                    DetectorIds = new string[] { detectorId },
+                    AdditionalParams = string.Empty,
+                }
+            };
+
+            response.Dataset.Add(diagnosticData);
+
+            return diagnosticData;
+        }
+
+        public static DiagnosticData AddDetector(this Response response, string detectorId, IDictionary<string, string> additionalParams = null)
         {
             var diagnosticData = new DiagnosticData()
             {
