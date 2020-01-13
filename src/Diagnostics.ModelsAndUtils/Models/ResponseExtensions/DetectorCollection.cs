3045ebda1a21ff6261c14492639b901ce1d02c77
Rekha Swaminathan <reswamin@microsoft.com>
2020-01-13 12:27:21 -0800

    Removed as optional param in overloaded method

--- src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/DetectorCollection.cs
+++ src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/DetectorCollection.cs
@@ -21,14 +21,14 @@ namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
             return diagnosticData;
         }
 
-        public static DiagnosticData AddDetectorCollection(this Response response, List<string> detectorIds, IDictionary<string, string> additionalParams = null)
+        public static DiagnosticData AddDetectorCollection(this Response response, List<string> detectorIds, IDictionary<string, string> additionalParams)
         {
             var diagnosticData = new DiagnosticData()
             {
                 RenderingProperties = new DetectorCollectionRendering()
                 {
                     DetectorIds = detectorIds,
-                    AdditionalParams = additionalParams != null ? JsonConvert.SerializeObject(additionalParams) : string.Empty,
+                    AdditionalParams = JsonConvert.SerializeObject(additionalParams),
                 }
             };
 
@@ -54,14 +54,14 @@ namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
             return diagnosticData;
         }
 
-        public static DiagnosticData AddDetector(this Response response, string detectorId, IDictionary<string, string> additionalParams = null)
+        public static DiagnosticData AddDetector(this Response response, string detectorId, IDictionary<string, string> additionalParams)
         {
             var diagnosticData = new DiagnosticData()
             {
                 RenderingProperties = new DetectorCollectionRendering()
                 {
                     DetectorIds = new string[] { detectorId },
-                    AdditionalParams = additionalParams != null ? JsonConvert.SerializeObject(additionalParams) : string.Empty,
+                    AdditionalParams = JsonConvert.SerializeObject(additionalParams),
                 }
             };
 
