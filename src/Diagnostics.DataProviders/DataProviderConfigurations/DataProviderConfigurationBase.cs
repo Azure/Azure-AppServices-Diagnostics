02820099f3f77a00dc58825dd3bec79b414078ee
Hawk Foreste <hawfor@microsoft.com>
2020-01-27 18:52:49 -0800

    Implement Observer health check

--- src/Diagnostics.DataProviders/DataProviderConfigurations/DataProviderConfigurationBase.cs
+++ src/Diagnostics.DataProviders/DataProviderConfigurations/DataProviderConfigurationBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using Kusto.Cloud.Platform.Utils;
 
@@ -7,11 +8,42 @@ namespace Diagnostics.DataProviders
 {
     public class DataProviderConfigurationBase : IDataProviderConfiguration
     {
+        private Dictionary<string, string> _healthCheckInputs;
+        private const int keyValuePairLength = 2;
+
+        /// <summary>
+        /// Feature flag for data provider.
+        /// </summary>
         [ConfigurationName("Enabled")]
         public bool Enabled { get; set; }
 
-        [ConfigurationName("HealchCheckInputs")]
-        public Dictionary<string, string> HealthCheckInputs { get; set; }
+        /// <summary>
+        /// A semi-colon delimitted lists of key value pairs. eg., key1=value1;key2=value2;key3=value3
+        /// </summary>
+        [ConfigurationName("HealthCheckInputs")]
+        public string HealthCheckInputsString { get; set; }
+
+        public Dictionary<string, string> HealthCheckInputs
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(HealthCheckInputsString) && _healthCheckInputs == null)
+                {
+                    _healthCheckInputs = new Dictionary<string, string>(StringComparer.CurrentCultureIgnoreCase);
+                    var keyValuePairs = HealthCheckInputsString.Trim().Split(new char[] { ';', ',', '|' });
+                    foreach (string pair in keyValuePairs)
+                    {
+                        var arr = pair.Trim().Split(new char[] { '=' });
+                        if(arr.Length == keyValuePairLength)
+                        {
+                            _healthCheckInputs.Add(arr[0], arr[1]);
+                        }
+                    }
+                }
+
+                return _healthCheckInputs;
+            }
+        }
 
         public virtual void PostInitialize() { }
     }
--- src/Diagnostics.DataProviders/Interfaces/IDataProviderConfiguration.cs
+++ src/Diagnostics.DataProviders/Interfaces/IDataProviderConfiguration.cs
@@ -6,9 +6,7 @@ namespace Diagnostics.DataProviders
     {
         bool Enabled { get; set; }
 
-#pragma warning disable CA2227 // Collection properties should be read only
-        Dictionary<string, string> HealthCheckInputs { get; set; }
-#pragma warning restore CA2227 // Collection properties should be read only
+        Dictionary<string, string> HealthCheckInputs { get; }
 
         void PostInitialize();
     }
