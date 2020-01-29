9d32c1fd26c6b31d1828118394d692dbbb7be6cd
Hao Hu Zhao <haohu@microsoft.com>
2020-01-28 16:50:36 -0800

    Add code coverage

--- restore.cmd
+++ restore.cmd
@@ -1,2 +1,14 @@
 pushd "%~dp0"
-dotnet restore "%~dp0..\AppServiceSample.sln" || exit /b 1
+
+dotnet restore "%~dp0..\AppServiceSample.sln"
+
+if %ERRORLEVEL% neq 0 (
+    popd
+    exit /B %ERRORLEVEL%
+)
+
+rem Install code coverage tool
+dotnet tool install dotnet-reportgenerator-globaltool --tool-path coveragetool
+
+popd
+exit /B %ERRORLEVEL%
--- test.cmd
+++ test.cmd
@@ -1,3 +1,13 @@
-cd /D "%~dp0"
-dotnet test AppServiceSample.sln --logger:trx
+pushd "%~dp0"
+
+dotnet test AppServiceSample.sln /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura --logger:trx
+
+if %ERRORLEVEL% neq 0 (
+    popd
+    exit /B %ERRORLEVEL%
+)
+
+coveragetool\reportgenerator -reports:**\coverage.cobertura.xml -targetdir:.\reports "-reportTypes:htmlInline;Cobertura
+
+popd
 exit /B %ERRORLEVEL%
