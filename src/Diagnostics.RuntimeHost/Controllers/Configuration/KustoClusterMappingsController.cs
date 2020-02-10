f901d3bad9decd8de4a9d733739314e622ff2612
Hawk Foreste <hawfor@microsoft.com>
2020-02-10 02:38:37 -0800

    Add kusto mappings configuration controller

---
+++ src/Diagnostics.RuntimeHost/Controllers/Configuration/KustoClusterMappingsController.cs
@@ -0,0 +1,45 @@
+﻿// <copyright file="KustoClusterMappingsController.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Diagnostics.RuntimeHost.Models;
+using Diagnostics.RuntimeHost.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Diagnostics.RuntimeHost.Controllers.Configuration
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route(UriElements.ArmResource + "/" + UriElements.KustoClusterMappings)]
+    public class KustoClusterMappingsController : Controller
+    {
+        public KustoClusterMappingsController()
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult AddOrUpdateMapping(string subscriptionId, string resourceGroupName, string provider, string resourceTypeName, string resourceName, [FromBody]TablePostBody kustoMappings)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet(UriElements.UniqueResourceId)]
+        public Task<IActionResult> GetMapping(string subscriptionId, string resourceGroupName, string provider, string resourceTypeName, string resourceName, string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete(UriElements.UniqueResourceId)]
+        public Task<IActionResult> DeleteMapping(string subscriptionId, string resourceGroupName, string provider, string resourceTypeName, string resourceName, string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Diagnostics.RuntimeHost/Models/TablePostBody.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diagnostics.RuntimeHost.Models
+{
+    public sealed class TablePostBody
+    {
+        public IEnumerable<string> Columns { get; set; }
+        public IEnumerable<string[]> Rows { get; set; }
+    }
+}
