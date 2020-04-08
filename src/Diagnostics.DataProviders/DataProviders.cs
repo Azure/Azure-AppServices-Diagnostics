f10fd98dd85bf35f0a15bd7c08c787130848e89d
Puneet Gupta <puneetg@microsoft.com>
2020-04-08 15:10:28 +0530

    Fixing DataSources issue for MDM

--- src/Diagnostics.DataProviders/DataProviders.cs
+++ src/Diagnostics.DataProviders/DataProviders.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Diagnostics.DataProviders.DataProviderConfigurations;
 using Diagnostics.DataProviders.Interfaces;
-
+using Diagnostics.ModelsAndUtils.Models;
 
 namespace Diagnostics.DataProviders
 {
@@ -18,27 +20,85 @@ namespace Diagnostics.DataProviders
         public Func<MdmDataSource, IMdmDataProvider> Mdm;
         public Func<GenericMdmDataProviderConfiguration, IMdmDataProvider> MdmGeneric;
 
+        private readonly Dictionary<object, IMetadataProvider> _dataProvidersCache = new Dictionary<object, IMetadataProvider>();
+
         public DataProviders(DataProviderContext context)
         {
-            Kusto = new KustoLogDecorator(context, new KustoDataProvider(_cache, context.Configuration.KustoConfiguration, context.RequestId, context.KustoHeartBeatService));
-            Observer = new ObserverLogDecorator(context, SupportObserverDataProviderFactory.GetDataProvider(_cache, context.Configuration, context));
-            GeoMaster = new GeoMasterLogDecorator(context, new GeoMasterDataProvider(_cache, context));
-            AppInsights = new AppInsightsLogDecorator(context, new AppInsightsDataProvider(_cache, context.Configuration.AppInsightsConfiguration));
-            ChangeAnalysis = new ChangeAnalysisLogDecorator(context, new ChangeAnalysisDataProvider(_cache, context.Configuration.ChangeAnalysisDataProviderConfiguration, context.RequestId, context.clientObjectId, context.clientPrincipalName, Kusto, context.receivedHeaders));
-            Asc = new AscLogDecorator(context, new AscDataProvider(_cache, context.Configuration.AscDataProviderConfiguration, context.RequestId, context));
-            Mdm = (MdmDataSource ds) =>
+
+            Kusto = FromMemoryCache("Kusto", context) as IKustoDataProvider;
+            Observer = FromMemoryCache("Observer", context) as ISupportObserverDataProvider;
+            GeoMaster = FromMemoryCache("GeoMaster", context) as IGeoMasterDataProvider;
+            AppInsights = FromMemoryCache("AppInsights", context) as IAppInsightsDataProvider;
+            ChangeAnalysis = FromMemoryCache("ChangeAnalysis", context) as IChangeAnalysisDataProvider;
+            Asc = FromMemoryCache("Asc", context) as IAscDataProvider;
+            Mdm = (MdmDataSource ds) => FromMemoryCache(ds, context) as IMdmDataProvider;
+            MdmGeneric = (GenericMdmDataProviderConfiguration config) => FromMemoryCache(config, context) as IMdmDataProvider;
+        }
+
+        private IMetadataProvider FromMemoryCache(object key, DataProviderContext context)
+        {
+            if (_dataProvidersCache.ContainsKey(key))
             {
-                switch (ds)
+                return _dataProvidersCache[key] as IMetadataProvider;
+            }
+            else
+            {
+                IMetadataProvider dataProviderObject = null;
+                if (key is string keyString)
                 {
-                    case MdmDataSource.Antares:
-                        return new MdmLogDecorator(context, new MdmDataProvider(_cache, context.Configuration.AntaresMdmConfiguration, context.RequestId));
-                    case MdmDataSource.Networking:
-                        return new MdmLogDecorator(context, new MdmDataProvider(_cache, context.Configuration.NetworkingMdmConfiguration, context.RequestId));
-                    default:
-                        throw new NotSupportedException($"{ds} is not supported.");
+                    switch (keyString)
+                    {
+                        case "Kusto":
+                            dataProviderObject = new KustoLogDecorator(context, new KustoDataProvider(_cache, context.Configuration.KustoConfiguration, context.RequestId, context.KustoHeartBeatService));
+                            break;
+                        case "Observer":
+                            dataProviderObject = new ObserverLogDecorator(context, SupportObserverDataProviderFactory.GetDataProvider(_cache, context.Configuration, context));
+                            break;
+                        case "GeoMaster":
+                            dataProviderObject = new GeoMasterLogDecorator(context, new GeoMasterDataProvider(_cache, context));
+                            break;
+                        case "AppInsights":
+                            dataProviderObject = new AppInsightsLogDecorator(context, new AppInsightsDataProvider(_cache, context.Configuration.AppInsightsConfiguration));
+                            break;
+                        case "ChangeAnalysis":
+                            dataProviderObject = new ChangeAnalysisLogDecorator(context, new ChangeAnalysisDataProvider(_cache, context.Configuration.ChangeAnalysisDataProviderConfiguration, context.RequestId, context.clientObjectId, context.clientPrincipalName, Kusto, context.receivedHeaders));
+                            break;
+                        case "Asc":
+                            dataProviderObject = new AscLogDecorator(context, new AscDataProvider(_cache, context.Configuration.AscDataProviderConfiguration, context.RequestId, context));
+                            break;
+                        default:
+                            break;
+                    }
                 }
-            };
-            MdmGeneric = (GenericMdmDataProviderConfiguration config) => new MdmLogDecorator(context, new MdmDataProvider(_cache, new GenericMdmDataProviderConfigurationWrapper(config), context.RequestId, context.receivedHeaders));
+                else if (key is MdmDataSource ds)
+                {
+                    switch (ds)
+                    {
+                        case MdmDataSource.Antares:
+                            dataProviderObject = new MdmLogDecorator(context, new MdmDataProvider(_cache, context.Configuration.AntaresMdmConfiguration, context.RequestId));
+                            break;
+                        case MdmDataSource.Networking:
+                            dataProviderObject = new MdmLogDecorator(context, new MdmDataProvider(_cache, context.Configuration.NetworkingMdmConfiguration, context.RequestId));
+                            break;
+                        default:
+                            throw new NotSupportedException($"{ds} is not supported.");
+                    }
+                }
+                else if (key is GenericMdmDataProviderConfiguration config)
+                {
+                    dataProviderObject = new MdmLogDecorator(context, new MdmDataProvider(_cache, new GenericMdmDataProviderConfigurationWrapper(config), context.RequestId, context.receivedHeaders));
+                }
+
+                _dataProvidersCache.Add(key, dataProviderObject);
+                return dataProviderObject;
+            }
+        }
+
+        public List<DataProviderMetadata> GetMetadata()
+        {
+
+            return _dataProvidersCache.Values.Where(d => d != null && d.GetMetadata() != null)
+                .Select(d => d.GetMetadata()).ToList();
         }
     }
 }
