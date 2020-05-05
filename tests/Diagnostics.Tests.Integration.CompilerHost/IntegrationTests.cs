027e334e70cec6af180aa44481390d29b01e2e10
Craig Maher <cmaher@microsoft.com>
2020-05-05 13:18:13 -0700

    Addressed PR comments.

--- tests/Diagnostics.Tests.Integration.CompilerHost/IntegrationTests.cs
+++ tests/Diagnostics.Tests.Integration.CompilerHost/IntegrationTests.cs
@@ -1,22 +1,23 @@
 using System;
 using Xunit;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Diagnostics;
 using System.Threading.Tasks;
 
-namespace Diagnostics.Tests.Integration.RuntimeHost
+namespace Diagnostics.Tests.Integration.CompilerHost
 {
-    public class UnitTest1 : IClassFixture<WebApplicationFactory<Diagnostics.RuntimeHost.Startup>>
+    public class IntegrationTests : IClassFixture<WebApplicationFactory<Diagnostics.CompilerHost.Startup>>
     {
-        private readonly WebApplicationFactory<Diagnostics.RuntimeHost.Startup> _factory;
+        private readonly WebApplicationFactory<Diagnostics.CompilerHost.Startup> _factory;
 
-        public UnitTest1(WebApplicationFactory<Diagnostics.RuntimeHost.Startup> factory)
+        public IntegrationTests(WebApplicationFactory<Diagnostics.CompilerHost.Startup> factory)
         {
             _factory = factory;
         }
 
         [Theory]
-        [InlineData("/healthping")]
-        public async Task Test1(string url)
+        [InlineData("/api/CompilerHost/healthping")]
+        public async Task HealthPingTest(string url)
         {
             WebApplicationFactoryClientOptions options = new WebApplicationFactoryClientOptions();
             // Arrange
@@ -27,7 +28,7 @@ namespace Diagnostics.Tests.Integration.RuntimeHost
 
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
-            Assert.Equal("application/json; charset=utf-8",
+            Assert.Equal("text/plain; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
     }
--- tests/Diagnostics.Tests.Integration.RuntimeHost/IntegrationTests.cs
+++ tests/Diagnostics.Tests.Integration.RuntimeHost/IntegrationTests.cs
@@ -1,23 +1,22 @@
 using System;
 using Xunit;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Diagnostics;
 using System.Threading.Tasks;
 
-namespace Diagnostics.Tests.Integration.CompilerHost
+namespace Diagnostics.Tests.Integration.RuntimeHost
 {
-    public class UnitTest1 : IClassFixture<WebApplicationFactory<Diagnostics.CompilerHost.Startup>>
+    public class IntegrationTests : IClassFixture<WebApplicationFactory<Diagnostics.RuntimeHost.Startup>>
     {
-        private readonly WebApplicationFactory<Diagnostics.CompilerHost.Startup> _factory;
+        private readonly WebApplicationFactory<Diagnostics.RuntimeHost.Startup> _factory;
 
-        public UnitTest1(WebApplicationFactory<Diagnostics.CompilerHost.Startup> factory)
+        public IntegrationTests(WebApplicationFactory<Diagnostics.RuntimeHost.Startup> factory)
         {
             _factory = factory;
         }
 
         [Theory]
-        [InlineData("/api/CompilerHost/healthping")]
-        public async Task Test1(string url)
+        [InlineData("/healthping")]
+        public async Task HealthPingTest(string url)
         {
             WebApplicationFactoryClientOptions options = new WebApplicationFactoryClientOptions();
             // Arrange
@@ -28,7 +27,7 @@ namespace Diagnostics.Tests.Integration.CompilerHost
 
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
-            Assert.Equal("text/plain; charset=utf-8",
+            Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
     }
