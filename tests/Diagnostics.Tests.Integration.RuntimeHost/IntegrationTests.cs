367cd98c1ba6f717fbd5d43fd023e00949d8fbcc
Craig Maher <cmaher@microsoft.com>
2020-05-06 16:28:01 -0700

    Made a ListDetectorsTest

--- tests/Diagnostics.Tests.Integration.RuntimeHost/IntegrationTests.cs
+++ tests/Diagnostics.Tests.Integration.RuntimeHost/IntegrationTests.cs
@@ -2,6 +2,10 @@ using System;
 using Xunit;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Diagnostics.ModelsAndUtils.Models;
 
 namespace Diagnostics.Tests.Integration.RuntimeHost
 {
@@ -29,6 +33,33 @@ namespace Diagnostics.Tests.Integration.RuntimeHost
             response.EnsureSuccessStatusCode(); // Status Code 200-299
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData("/subscriptions/ef90e930-9d7f-4a60-8a99-748e0eea69de/resourceGroups/detector-development/providers/Microsoft.Web/sites/buggybakery/detectors")]
+        public async Task ListDetectorsTest(string url)
+        {
+            WebApplicationFactoryClientOptions options = new WebApplicationFactoryClientOptions();
+            // Arrange
+            var client = _factory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            
+            HttpContent content = new StringContent(string.Empty, System.Text.Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await client.PostAsync(url, content);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+
+            // Act
+            string responseString = await response.Content.ReadAsStringAsync();
+            var detectors = JsonConvert.DeserializeObject<DiagnosticApiResponse[]>(responseString);
+
+            // Assert
+            Assert.True(detectors.Length > 5);
         }
     }
 }
