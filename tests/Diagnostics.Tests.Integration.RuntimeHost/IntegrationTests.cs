a0f8d31e3015ffd6d752b93fd9bd63189c29ccd0
Craig Maher <cmaher@microsoft.com>
2020-05-12 12:04:28 -0700

    committing changes even though they are not ready

--- tests/Diagnostics.Tests.Integration.RuntimeHost/IntegrationTests.cs
+++ tests/Diagnostics.Tests.Integration.RuntimeHost/IntegrationTests.cs
@@ -6,6 +6,8 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 using Newtonsoft.Json;
 using Diagnostics.ModelsAndUtils.Models;
+using Microsoft.AspNetCore.Hosting;
+using System.Diagnostics;
 
 namespace Diagnostics.Tests.Integration.RuntimeHost
 {
@@ -63,6 +65,69 @@ namespace Diagnostics.Tests.Integration.RuntimeHost
         [InlineData("/subscriptions/ef90e930-9d7f-4a60-8a99-748e0eea69de/resourceGroups/detector-development/providers/Microsoft.Web/sites/buggybakery/detectors/httpservererrors")]
         public async Task CallDetectorTest(string url)
         {
+            // Arrange
+            var client = _factory.CreateClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpContent content = new StringContent(string.Empty, System.Text.Encoding.UTF8, "application/json");
+
+            // Act
+            var response = await client.PostAsync(url, content);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("application/json; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+
+            // Act
+            string responseString = await response.Content.ReadAsStringAsync();
+            var detector = JsonConvert.DeserializeObject<DiagnosticApiResponse>(responseString);
+
+            // Assert
+            Assert.True(detector != null);
+            Assert.True(detector.Dataset.Count >= 4);
+        }
+
+        private async Task LaunchCompilerHostProcess(string requestId)
+        {
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    WorkingDirectory = _compilerHostBinaryLocation,
+                    FileName = "dotnet",
+                    Arguments = $@"Diagnostics.CompilerHost.dll --urls {_compilerHostUrl}",
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = false
+                }
+            };
+
+            DiagnosticsETWProvider.Instance.LogCompilerHostClientMessage(requestId ?? string.Empty, _eventSource, $"Launching Compiler Host Process. Running \"{proc.StartInfo.FileName} {proc.StartInfo.Arguments}\"");
+
+            proc.Start();
+            await WaitForCompilerHostToBeReady(requestId);
+
+            if (!proc.HasExited)
+            {
+                _isComplierHostRunning = true;
+                _processId = proc.Id;
+                DiagnosticsETWProvider.Instance.LogCompilerHostClientMessage(requestId, _eventSource, $"Compiler Host Process running with Pid : {_processId}");
+            }
+            else
+            {
+                string standardOutput = await proc.StandardOutput.ReadToEndAsync();
+                DiagnosticsETWProvider.Instance.LogCompilerHostClientException(requestId, _eventSource, $"Failed to start Compiler Host Process. Standard Output : {standardOutput}", string.Empty, string.Empty);
+            }
+        }
+
+        [Theory]
+        [InlineData("/subscriptions/ef90e930-9d7f-4a60-8a99-748e0eea69de/resourceGroups/detector-development/providers/Microsoft.Web/sites/buggybakery/detectors/httpservererrors")]
+        public async Task CompileTest(string url)
+        {
+            // Start Compiler Host
+
+
+
             // Arrange
             var client = _factory.CreateClient();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
