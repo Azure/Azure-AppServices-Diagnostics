c898b5c7cc86f067abc592a02ea9889f87e2b716
Hawk Foreste <hawfor@microsoft.com>
2020-06-05 16:23:24 -0700

    Update build pipeline

--- publish.cmd
+++ publish.cmd
@@ -10,12 +10,15 @@ exit /b %errorlevel%
 )
 echo\
 
+powershell Compress-Archive -Path "%~dp0build\antares.external.diagnostics.compilerhost.1.0.0\*" -DestinationPath "%~dp0build\DiagnosticsCompilerHost.zip"
+
 :: Publish Runtime Host to Build Location
 echo\
 echo "------------------- Publishing Runtime Host to build directory -------------------"
 echo\
 dotnet publish "%~dp0src\Diagnostics.RuntimeHost\Diagnostics.RuntimeHost.csproj" -c Release -o "%~dp0build\antares.external.diagnostics.runtimehost.1.0.0"
 
+powershell Compress-Archive -Path "%~dp0build\antares.external.diagnostics.runtimehost.1.0.0\*" -DestinationPath "%~dp0build\DiagnosticsRuntimeHost.zip"
 
 IF %ERRORLEVEL% NEQ 0 (
 echo "Diagnostics.RuntimeHost Publish Failed."
