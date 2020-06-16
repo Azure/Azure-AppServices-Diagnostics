5ec84b000a3c7f6e82d03c4a9666ac317aa713e3
Hawk Foreste <hawfor@microsoft.com>
2020-06-16 14:14:23 -0700

    Create nullable storage (#528)
    * Create extension methods for diagentity storage services. Instantiate different implementations based on cloud environment.
    
    * Refacture logic to determine if public azure cloud environment.

---
+++ src/Diagnostics.RuntimeHost/Extensions/MultiCloudExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Diagnostics.DataProviders;
+
+namespace Microsoft.Extensions.Configuration
+{
+    public static class MultiCloudExtensions
+    {
+        /// <summary>
+        /// Determines if this cloud is Public Azure cloud.
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        public static bool IsPublicAzure(this IConfiguration configuration)
+        {
+            return configuration.GetValue<string>("CloudDomain").Equals(DataProviderConstants.AzureCloud, StringComparison.CurrentCultureIgnoreCase)
+                || configuration.GetValue<string>("CloudDomain").Equals(DataProviderConstants.AzureCloudAlternativeName, StringComparison.CurrentCultureIgnoreCase);
+        }
+    }
+}
