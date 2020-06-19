3a40498dc34d36664d0f493a9d7f910a2efc205c
Rekha Swaminathan <reswamin@microsoft.com>
2020-06-19 16:23:14 -0700

    Remove unused reference

--- src/Diagnostics.RuntimeHost/Services/GithubClient/GithubEntry.cs
+++ src/Diagnostics.RuntimeHost/Services/GithubClient/GithubEntry.cs
@@ -2,9 +2,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 // </copyright>
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-
 namespace Diagnostics.RuntimeHost.Services
 {
     /// <summary>
