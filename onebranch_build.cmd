d0077eac862a2f36b4baa97a4ea0474f419b9c31
Hawk Foreste <hawfor@microsoft.com>
2020-06-24 12:05:27 -0700

    updated wrong build script

--- onebranch_build.cmd
+++ onebranch_build.cmd
@@ -3,8 +3,13 @@ pushd "%~dp0"
 
 :: Build all the projects in the solution
 ::dotnet build --no-incremental --no-restore -c Release -v detailed %~dp0Diagnostics.sln"
-dotnet build --no-incremental --no-restore -c Release -v detailed %~dp0src\Diagnostics.RuntimeHost\Diagnostics.RuntimeHost.csproj
-dotnet build --no-incremental --no-restore -c Release -v detailed %~dp0src\Diagnostics.CompilerHost\Diagnostics.CompilerHost.csproj
+
+echo "Check if Node is installed"
+npm help
+npm --version
+
+dotnet build --no-incremental --no-restore -c Release -v minimal %~dp0src\Diagnostics.RuntimeHost\Diagnostics.RuntimeHost.csproj
+dotnet build --no-incremental --no-restore -c Release -v minimal %~dp0src\Diagnostics.CompilerHost\Diagnostics.CompilerHost.csproj
 
 IF %ERRORLEVEL% NEQ 0 (
 echo "Build Failed."
