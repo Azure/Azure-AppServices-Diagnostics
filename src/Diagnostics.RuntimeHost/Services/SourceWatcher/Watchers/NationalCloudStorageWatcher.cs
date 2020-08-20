d4d036c24133166f36ef568ee7c763cf8e5a751b
Hawk Foreste <hawfor@microsoft.com>
2020-08-20 14:28:06 -0700

    Revert NationalCloudStorageWatcher

---
+++ src/Diagnostics.RuntimeHost/Services/SourceWatcher/Watchers/NationalCloudStorageWatcher.cs
@@ -0,0 +1,22 @@
+﻿using Diagnostics.RuntimeHost.Models;
+using Diagnostics.RuntimeHost.Services.CacheService;
+using Diagnostics.RuntimeHost.Services.StorageService;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Threading.Tasks;
+
+namespace Diagnostics.RuntimeHost.Services.SourceWatcher.Watchers
+{
+    public sealed class NationalCloudStorageWatcher : StorageWatcher
+    {
+        public NationalCloudStorageWatcher(IHostingEnvironment env, IConfiguration configuration, IStorageService service, IInvokerCacheService invokerCacheService, IGistCacheService gistCacheService, IKustoMappingsCacheService kustoMappingsCacheService): base(env, configuration, service, invokerCacheService, gistCacheService, kustoMappingsCacheService)
+        {
+
+        }
+
+        public override Task CreateOrUpdatePackage(Package pkg)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
