6ece85d5c9787f0899d9d8cf295d0685003f96fb
Ajay Sharma <ajsharm@microsoft.com>
2020-10-29 19:50:50 -0700

    Add feature for excluding sites from web search

--- src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/SearchComponent.cs
+++ src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/SearchComponent.cs
@@ -23,11 +23,13 @@ namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
         public int MaxResults { get; set; }
         public bool UseStack { get; set; }
         public List<string> PreferredSites { get; set; }
-        public WebSearchConfiguration(int maxResults = 5, bool useStack=true, List<string> preferredSites = null)
+        public List<string> ExcludeSites { get; set; }
+        public WebSearchConfiguration(int maxResults = 5, bool useStack=true, List<string> preferredSites = null, List<string> excludeSites = null)
         {
             MaxResults = maxResults;
             UseStack = useStack;
             PreferredSites = preferredSites!=null? preferredSites: new List<string>();
+            ExcludeSites = excludeSites != null ? excludeSites : new List<string>();
         }
     }
     public static class ResponseSearchComponentExtension
