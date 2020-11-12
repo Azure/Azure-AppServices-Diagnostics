49f121f0488d1247f5e182026a5e0667915bfca6
Ajay Sharma <ajsharm@microsoft.com>
2020-11-11 16:10:26 -0800

    Model

--- src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/KeystoneComponent.cs
+++ src/Diagnostics.ModelsAndUtils/Models/ResponseExtensions/KeystoneComponent.cs
@@ -6,24 +6,43 @@ using System.Text;
 
 namespace Diagnostics.ModelsAndUtils.Models.ResponseExtensions
 {
+    public class KeystoneInsight
+    {
+        public string Title;
+        public string Summary;
+        public Solution Solution;
+        public DateTime StartDate;
+        public DateTime ExpiryDate;
+
+        public KeystoneInsight(string title, string summary, Solution solution=null, DateTime? startDate= null, DateTime? expiryDate = null)
+        {
+            Title = title;
+            Summary = summary;
+            Solution = solution;
+            StartDate = startDate ?? DateTime.UtcNow;
+            ExpiryDate = expiryDate ?? DateTime.MaxValue;
+        }
+    }
+
     public static class ResponseKeystoneComponentExtension
     {
-        public static DiagnosticData AddKeystoneComponent(this Response response, object keystoneInsight)
+        public static DiagnosticData AddKeystoneComponent(this Response response, KeystoneInsight keystoneInsight)
         {
             try
             {
-                var loggingName = keystoneInsight.GetType().GetProperty("LoggingName");
-                var title = keystoneInsight.GetType().GetProperty("Title");
-                var summary = keystoneInsight.GetType().GetProperty("Summary");
-                if (!(loggingName != null && loggingName.GetValue(keystoneInsight) != null && title != null && title.GetValue(keystoneInsight) != null && summary != null && summary.GetValue(keystoneInsight) != null))
+                if (!(keystoneInsight.Title != null && keystoneInsight.Summary != null))
                 {
-                    throw new Exception("Required attributes LoggingName, Title, and Summary cannot be null for KeystoneInsight");
+                    throw new Exception("Required attributes Title, and Summary cannot be null for KeystoneInsight.");
                 }
+                var table = new DataTable();
+                table.Columns.Add("Content", typeof(string));
+                DataRow newRow = table.NewRow();
+                newRow["Content"] = JsonConvert.SerializeObject(keystoneInsight);
+                table.Rows.Add(newRow);
                 var diagData = new DiagnosticData()
                 {
-                    RenderingProperties = new Rendering(RenderingType.KeystoneComponent) {
-                        Description = JsonConvert.SerializeObject(keystoneInsight)
-                    }
+                    Table = table,
+                    RenderingProperties = new Rendering(RenderingType.KeystoneComponent)
                 };
                 response.Dataset.Add(diagData);
                 return diagData;
