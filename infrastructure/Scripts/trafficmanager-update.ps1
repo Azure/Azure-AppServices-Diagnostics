52b5ace88d941d6fe8f423df0094de179860068f
Darrel Donald <darreldonald@microsoft.com>
2021-05-27 17:10:41 -0700

    Added trafficmanager-update script

---
+++ infrastructure/Scripts/trafficmanager-update.ps1
@@ -0,0 +1,48 @@
+﻿# Parameters defined
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Subscription,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$TrafficManager,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$TrafficManagerResourceGroup,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$Endpoint,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [string]$EndpointType,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateNotNullOrEmpty()]
+    [bool]$TrafficEnabled
+)
+
+
+Write-Host "Setting subscription context: " $Subscription
+#set the subscripton context
+Set-AzContext -SubscriptionId $Subscription
+
+Write-Host "Get TrafficManager Details for: " $TrafficManager
+$trafficmanagerDetails = Get-AzTrafficManagerProfile -ResourceGroupName $TrafficManagerResourceGroup -Name $TrafficManager
+
+if($trafficmanagerDetails) {
+
+    if ($TrafficEnabled){
+        Enable-AzTrafficManagerEndpoint -Name $Endpoint -Type $EndpointType -ProfileName $TrafficManager -ResourceGroupName $TrafficManagerResourceGroup 
+    }
+    else {
+        Disable-AzTrafficManagerEndpoint -Name $Endpoint -Type $EndpointType -ProfileName $TrafficManager -ResourceGroupName $TrafficManagerResourceGroup -Force
+    }
+    Write-Host "Updated frontdoor settings for " $TrafficManager
+
+} else {
+    Write-Host "Could not retrieve frontdoor details for: " $TrafficManager
+}
