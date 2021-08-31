2096259ed387b2bb283c1eb323e86d96db699b12
yifguoMSFT <68874830+yifguoMSFT@users.noreply.github.com>
2021-08-31 10:13:40 -0700

    Enable query shadowing for kusto provider (#729)

--- src/Diagnostics.DataProviders/Interfaces/IKustoClient.cs
+++ src/Diagnostics.DataProviders/Interfaces/IKustoClient.cs
@@ -1,13 +1,14 @@
-﻿using System.Data;
+﻿using System;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace Diagnostics.DataProviders
 {
     public interface IKustoClient
     {
-        Task<DataTable> ExecuteQueryAsync(string query, string cluster, string database, int timeoutSeconds, string requestId = null, string operationName = null);
+        Task<DataTable> ExecuteQueryAsync(string query, string cluster, string database, int timeoutSeconds, string requestId = null, string operationName = null, DateTime? startTime = null, DateTime? endTime = null);
 
-        Task<DataTable> ExecuteQueryAsync(string query, string cluster, string database, string requestId = null, string operationName = null);
+        Task<DataTable> ExecuteQueryAsync(string query, string cluster, string database, string requestId = null, string operationName = null, DateTime? startTime = null, DateTime? endTime = null);
 
         Task<KustoQuery> GetKustoQueryAsync(string query, string cluster, string database);
 
