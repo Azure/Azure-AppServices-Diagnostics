2824a8f6ec6e460641e153498f0deaac034ad46d
Darrel Donald <darreldonald@microsoft.com>
2021-10-27 14:15:41 -0700

    .

---
+++ src/Diagnostics.RuntimeHost/Services/DevOpsClient/NationalCloudDevOpsClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diagnostics.RuntimeHost.Services.DevOpsClient
+{
+    public class NationalCloudDevOpsClient : IRepoClient
+    {
+        public Task<object> GetBranchesAsync(string resourceUri, string requestId)
+        {
+            return null;
+        }
+
+        public Task<object> GetFileContentAsync(string filePathInRepo, string resourceUri, string requestId, string branch = null)
+        {
+            return null;
+        }
+
+        public Task<object> MakePullRequestAsync(string sourceBranch, string targetBranch, string title, string resourceUri, string requestId)
+        {
+            return null;
+        }
+
+        public Task<object> PushChangesAsync(string branch, string file, string repoPath, string comment, string changeType, string resourceUri, string requestId)
+        {
+            return null;
+        }
+    }
+}
