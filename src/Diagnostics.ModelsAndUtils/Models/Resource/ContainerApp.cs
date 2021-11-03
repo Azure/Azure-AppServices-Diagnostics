9b66aeb236d9b71c64e473992d921b4eb9223483
Ajay Sharma <SharmaAjay19@users.noreply.github.com>
2021-11-03 11:47:31 -0700

    Correcting the FQDN property name in resource

--- src/Diagnostics.ModelsAndUtils/Models/Resource/ContainerApp.cs
+++ src/Diagnostics.ModelsAndUtils/Models/Resource/ContainerApp.cs
@@ -17,7 +17,7 @@ namespace Diagnostics.ModelsAndUtils.Models
         /// <summary>
         /// FQDN of the Container App
         /// </summary>
-        public string Fdqn { get; set; }
+        public string Fqdn { get; set; }
 
         /// <summary>
         /// Subscription Id(Guid)
@@ -77,14 +77,14 @@ namespace Diagnostics.ModelsAndUtils.Models
             get; set;
         }
 
-        public ContainerApp(string subscriptionId, string resourceGroup, string resourceName, string kubeEnvironmentName=null, string geoMasterName=null, string fdqn=null, string subLocationPlacementId = null) : base()
+        public ContainerApp(string subscriptionId, string resourceGroup, string resourceName, string kubeEnvironmentName=null, string geoMasterName=null, string fqdn=null, string subLocationPlacementId = null) : base()
         {
             this.SubscriptionId = subscriptionId;
             this.ResourceGroup = resourceGroup;
             this.Name = resourceName;
             this.KubeEnvironmentName = kubeEnvironmentName;
             this.GeoMasterName = geoMasterName;
-            this.Fdqn = fdqn;
+            this.Fqdn = fqdn;
             SubscriptionLocationPlacementId = subLocationPlacementId;
         }
 
