ccd92c3760510508dcd64d43c77a8a36efa6773d
Puneet Gupta <puneetg@microsoft.com>
2021-12-03 22:47:04 +0530

    correcting hyperlink

--- src/Diagnostics.RuntimeHost/SerializationConvertors/AsRuntimeTypeConverter.cs
+++ src/Diagnostics.RuntimeHost/SerializationConvertors/AsRuntimeTypeConverter.cs
@@ -5,7 +5,7 @@ using System.Text.Json.Serialization;
 namespace Diagnostics.RuntimeHost
 {
     /// <summary>
-    /// Converter copied from https://newbedev.com/derived-type-s-properties-missing-in-json-response-from-asp-net-core-api
+    /// Converter copied from https://stackoverflow.com/questions/59308763/derived-types-properties-missing-in-json-response-from-asp-net-core-api
     /// This is required to ensure all the derived classes get serialized properly by System.Text.Json
     /// </summary>
     /// <typeparam name="T"></typeparam>
