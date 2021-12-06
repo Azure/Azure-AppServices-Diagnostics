a533a0546e75a056f6bcec14b83e1a5098191add
Puneet Gupta <puneetg@microsoft.com>
2021-12-06 22:54:29 +0530

    switching to camel casing for DeploymentController

--- src/Diagnostics.ModelsAndUtils/Models/Storage/DeploymentParameters.cs
+++ src/Diagnostics.ModelsAndUtils/Models/Storage/DeploymentParameters.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Text.Json.Serialization;
 
 namespace Diagnostics.ModelsAndUtils.Models.Storage
 {
@@ -11,37 +10,31 @@ namespace Diagnostics.ModelsAndUtils.Models.Storage
         /// <summary>
         /// Single commit id to deploy. Not applicable if <see cref="FromCommitId"/> and <see cref="ToCommitId"/> is given.
         /// </summary>
-        [JsonPropertyName("CommitId")]
         public string CommitId { get; set; }
 
         /// <summary>
         /// Start commit to deploy. Not applicable if <see cref="CommitId"/> is given.
         /// </summary>
-        [JsonPropertyName("FromCommitId")]
         public string FromCommitId { get; set; }
 
         /// <summary>
         /// End commit to deploy. Not applicable if <see cref="CommitId"/> is given.
         /// </summary>
-        [JsonPropertyName("ToCommitId")]
         public string ToCommitId { get; set; }
 
         /// <summary>
         /// If provided, includes detectors modified after this date. Cannot be combined with <see cref="FromCommitId"/> and <see cref="ToCommitId"/>.
         /// </summary>
-        [JsonPropertyName("StartDate")]
         public string StartDate { get; set; }
 
         /// <summary>
         /// If provided, includes detectors modified before this date. Cannot be combined with <see cref="FromCommitId"/> and <see cref="ToCommitId"/>.
         /// </summary>
-        [JsonPropertyName("EndDate")]
         public string EndDate { get; set; }
 
         /// <summary>
         /// Resource type of the caller. eg. Microsoft.Web/sites.
         /// </summary>
-        [JsonPropertyName("ResourceType")]
         public string ResourceType { get; set; }
     }
 
@@ -53,29 +46,21 @@ namespace Diagnostics.ModelsAndUtils.Models.Storage
         /// <summary>
         /// List of detectors that got updated/added/edited;
         /// </summary>
-
-        [JsonPropertyName("DeployedDetectors")]
         public List<string> DeployedDetectors { get; set; }
 
         /// <summary>
         /// List of detectors that failed deployment along with the reason of failure;
         /// </summary>
-
-        [JsonPropertyName("FailedDetectors")]
         public Dictionary<string, string> FailedDetectors { get; set; }
 
         /// <summary>
         /// List of detectors that were marked for deletion;
         /// </summary>
-
-        [JsonPropertyName("DeletedDetectors")]
         public List<string> DeletedDetectors { get; set; }
 
         /// <summary>
         /// Unique Guid to track the deployment;
         /// </summary>
-
-        [JsonPropertyName("DeploymentGuid")]
         public string DeploymentGuid { get; set; }
     }
 }
