ead75e148c754410a0458b0080a74934d50d0caa
Michimune Kohno <mikono@microsoft.com>
2021-12-09 22:35:32 -0800

    Fix DynamicInsightRendering rendering

--- src/Diagnostics.ModelsAndUtils/Models/QueryResponse.cs
+++ src/Diagnostics.ModelsAndUtils/Models/QueryResponse.cs
@@ -4,11 +4,11 @@ namespace Diagnostics.ModelsAndUtils.Models
 {
     public class QueryResponse<T>
     {
-        public CompilerResponse CompilationOutput;
-        public IEnumerable<RuntimeLogEntry> RuntimeLogOutput;
+        public CompilerResponse CompilationOutput { get; set; }
+        public IEnumerable<RuntimeLogEntry> RuntimeLogOutput { get; set; }
 
-        public bool RuntimeSucceeded;
+        public bool RuntimeSucceeded { get; set; }
 
-        public T InvocationOutput;
+        public T InvocationOutput { get; set; }
     }
 }
