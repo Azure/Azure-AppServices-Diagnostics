a760109366d368fe68f5443ad104477a9ae4fd2a
michimune <michimune@outlook.com>
2021-12-11 16:03:41 -0800

    Fix runtime exception incorrectly shown in the runtime logs (#799)
    * Fix runtime exception incorrectly shown in the runtime logs
    
    * Add InnerException property
    
    Co-authored-by: Michimune Kohno <mikono@microsoft.com>

--- src/Diagnostics.RuntimeHost/Startup.cs
+++ src/Diagnostics.RuntimeHost/Startup.cs
@@ -149,7 +149,7 @@ namespace Diagnostics.RuntimeHost
                 }).AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.WriteIndented = true;
-                    options.JsonSerializerOptions.Converters.Add(new AsPrintableTypeConverter<Exception>());
+                    options.JsonSerializerOptions.Converters.Add(new ExceptionConverter());
                     options.JsonSerializerOptions.Converters.Add(new AsRuntimeTypeConverter<Rendering>());
                     options.JsonSerializerOptions.Converters.Add(new AsRuntimeTypeConverter<ModelsAndUtils.Models.ResponseExtensions.FormInputBase>());
                 });
@@ -159,7 +159,7 @@ namespace Diagnostics.RuntimeHost
                 services.AddControllers().AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.WriteIndented = true;
-                    options.JsonSerializerOptions.Converters.Add(new AsPrintableTypeConverter<Exception>());
+                    options.JsonSerializerOptions.Converters.Add(new ExceptionConverter());
                     options.JsonSerializerOptions.Converters.Add(new AsRuntimeTypeConverter<Rendering>());
                     options.JsonSerializerOptions.Converters.Add(new AsRuntimeTypeConverter<ModelsAndUtils.Models.ResponseExtensions.FormInputBase>());
                 });
