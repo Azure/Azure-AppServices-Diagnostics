eada46a46317d969f366be81c3dbfd4e4682b146
Darrel Donald <darreldonald@microsoft.com>
2022-03-04 16:34:23 -0800

    send org, proj, and repo in config api

--- src/Diagnostics.RuntimeHost/Controllers/DevOpsController.cs
+++ src/Diagnostics.RuntimeHost/Controllers/DevOpsController.cs
@@ -88,10 +88,10 @@ namespace Diagnostics.RuntimeHost.Controllers
             ResourceProviderRepoConfig resourceProviderRepoConfig = await _devOpsClient.GetRepoConfigsAsync(resourceProviderType);
             if (resourceProviderRepoConfig == null)
             {
-                var noConfigResponse = new { GraduationEnabled = false, AutoMerge = false };
+                var noConfigResponse = new { GraduationEnabled = false, AutoMerge = false, Organization = "", Repository = "", Project = "" };
                 return Ok(noConfigResponse);
             }
-            var response = new { GraduationEnabled = true, AutoMerge = resourceProviderRepoConfig.AutoMerge };
+            var response = new { GraduationEnabled = true, AutoMerge = resourceProviderRepoConfig.AutoMerge, Organization = resourceProviderRepoConfig.Organization, Repository = resourceProviderRepoConfig.Repository, Project = resourceProviderRepoConfig.Project };
             return Ok(response);
         }
 
